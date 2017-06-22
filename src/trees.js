977570f3c77b827bc39f7b72cec8d5ef7aae6d90
johnsimerlink <johnsimerlink@gmail.com>
2017-06-21 21:51:57 -0700

    trying to print all static facts

--- src/trees.js
+++ src/trees.js
@@ -1,7 +1,13 @@
 /**
  * Created by John on 6/21/2017.
  */
-
+import {Facts} from './facts.js'
+console.log('trees.js imported')
+Facts.getAll((facts) => {
+   Object.keys(facts).forEach((factId) => {
+       console.log('factId ' + factId + ' has obj of ' + facts[factId])
+   })
+})
 const trees = {
     "1": {
 
