eccc17cc9588f193542cd52924c37de7628cd9bf
JohnSimerlink <johnsimerlink@gmail.com>
2017-06-22 21:53:41 -0700

    merge conflict

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,10 @@
 
 </style>
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <h2> This is Renaissance </h2>
-  <hello color='blue'></hello>
-  <facts></facts>
-   {{msg}}
-  </div>
-=======
    <div id="app">
       <facts id="facts"></facts>
       <!--<trees-graph id="trees-graph"></trees-graph>-->
    </div>
->>>>>>> cbb1e924fb3d4993a2ee3db21aa21de143f043d4
 </template>
 
 <script>
@@ -37,14 +28,9 @@ export default {
     }
   },
   components: {
-<<<<<<< HEAD
     Hello
-    , SampleVue
     , Facts
-=======
-    Facts
 //    , TreesGraph
->>>>>>> cbb1e924fb3d4993a2ee3db21aa21de143f043d4
   }
 }
 </script>
