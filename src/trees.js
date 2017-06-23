cbb1e924fb3d4993a2ee3db21aa21de143f043d4
johnsimerlink <johnsimerlink@gmail.com>
2017-06-22 17:28:10 -0700

    offline trees constructor work in progress, for dev testing only when im on hte caltrain w/o internet access lol

--- src/trees.js
+++ src/trees.js
@@ -6,6 +6,7 @@ console.log('trees.js imported')
 getAllFacts((facts) => {
    Object.keys(facts).forEach((factId) => {
        console.log('factId ' + factId + ' has obj of ' + facts[factId])
+    const tree = new Tree(factId, null)
    })
 })
 const trees = {
