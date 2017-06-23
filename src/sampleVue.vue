62f4c9e39a3bdc8ff619fbe75060552394500947
JohnSimerlink <johnsimerlink@gmail.com>
2017-06-22 21:53:41 -0700

    merge conflict

--- src/sampleVue.vue
+++ src/sampleVue.vue
@@ -1,28 +1,26 @@
-// simple vue just for practice
+<template>
+    <div id="app">
+        <input v-model="message"><br />
+        {{ message }}
+        <ul>
+            <li v-for="doggo in doggos">{{ doggo }}</li>
+        </ul>
+        <button v-on:click="buttonClick">marco</button>
+    </div>
+</template>
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.4/vue.min.js"> </script>
-+    <title>Learning Vue.js! :D</title>
-+  </head>
-+  <body>
-+    <div id="app">
-+      <input v-model="message"><br />
-+      {{ message }}
-+      <ul>
-+        <li v-for="doggo in doggos">{{ doggo }}</li>
-+      </ul>
-+      <button v-on:click="buttonClick">marco</button>
-+    </div>
-+    <script>
-+      new Vue({
-+        el: '#app',
-+        data: {
-+          'message': 'Hello World',
-+          'doggos': ['shiba inu', 'golden retriever']
-+        },
-+        methods: {
-+          buttonClick: function() {
-+            alert('polo')
-+          }
-+        }
-+      });
-+    </script>
+<script>
+//  new Vue({
+//    el: '#app',
+//    data: {
+//      'message': 'Hello World',
+//      'doggos': ['shiba inu', 'golden retriever']
+//    },
+//    methods: {
+//      buttonClick: function() {
+//        alert('polo')
+//      }
+//    }
+//  });
+    //^^Shaffan, modify the above code so that it follows the same structure/pattern as that of hello.vue :)
+</script>
