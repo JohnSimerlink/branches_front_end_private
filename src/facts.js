47fd9f1f208af0976943739aa488e47fd4b81ffa
JohnSimerlink <johnsimerlink@gmail.com>
2017-06-23 20:59:50 -0700

    graph reads nodes from list now

--- src/facts.js
+++ src/facts.js
@@ -10,16 +10,19 @@ const factsArr = [
     {question: "What is a technique to treat other people warmly?", answer: "Imagine they have halos around their head, and that they are the best people/angels you have ever encountered"},
     {question: "How can you treat jerks nicely?", answer: "Change how you see their situation in your head (e.g. say to yourself 'That guy was just diagnosed with cancer'"},
     {question: "What is presence?", answer: "The state of being totally focused and aware on the current moment and place. Not on the future or past. Not thinking about other people/activities, but think about the people you are with now and the activity you are doing now."},
-    {question: "What is a technique to instantly get present?", answer: "Focus on your toes, and trying to feel your toes in your shoes (this brings attention to your current senses"}
+    {question: "What is a technique to instantly get present?", answer: "Focus on your toes, and trying to feel your toes in your shoes (this brings attention to your current senses)"}
 ]
 const facts = {}
 factsArr.forEach((fact) => {
-    const f = new Fact(fact);
+    const f = new Fact(fact.question, fact.answer);
     facts[f.id] = f;
 })
-export function getAllFacts(success) {
-    success(facts)
-}
-export function getFact(id, success){
-    success(facts[id])
+export class Facts {
+   static getAll(success) {
+     success(facts)
+   }
+   static get(factId, success){
+       const fact = facts[factId]
+       success(fact)
+   }
 }
