82a3c945f0edec1c5fb9f303480ef2ffda9770b5
Shaffan Mustafa <shaffan1996@gmail.com>
2017-06-24 16:28:54 -0400

    working on making a create account/login template

---
+++ src/newlogin.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<h2> Log In! </h2>
+<h2> Don't have an account? Sign up! </h2>
+</div>
+</template>
+<script>
+function clickCreateAccountButton() {
+    // should make username + password dissapear, replace with message that verifcation email has been sent,
+    // also display logout button
+    alert("A verification email has been sent to your email account. Please also check your spam/junk folder just to be safe!");
+    <buttton id='logout'>Log Out</button>
+}
+
+function clickSignInButton() {
+  var loggedin = true;
+}
+
+function clickLogoutButton() {
+  alert()
+}
+
+function validatePassword() {
+
+}
+
+function validateEmail() {
+
+}
+
+  export default {
+    name: 'newlogin',
+    data() {
+      return {}
+    },
+    methods: {
+
+    }
+}
+</script>
