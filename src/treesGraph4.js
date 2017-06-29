ef4999f5ee774052a8c9de6a04baa4c839f381ad
JohnSimerlink <johnsimerlink@gmail.com>
2017-06-28 20:58:54 -0700

    drag node and load root tree working

--- src/treesGraph4.js
+++ src/treesGraph4.js
@@ -10,21 +10,54 @@ var i,
     };
 
 // Generate a random graph:
-for (i = 0; i < N; i++){
-    g.nodes.push({
-        id: 'n' + i,
-        label: 'Node ' + i,
-        x: 100 * i,
-        y: 100 * i,
-        size: 10,
-        color: '#666'
-    });
-    console.log(g.nodes)
-}
 
+loadTreeAndSubTrees(1)
 // Instantiate sigma:
-s = new sigma({
-    graph: g,
-    container: 'graph-container'
-});
+function loadTreeAndSubTrees(treeId){
+    console.log('loadTreeAndSubTrees just called')
+
+    Trees.get(treeId, function(tree){
+        console.log("TREESGRAPH2.JS: Trees.get callback tree is", tree)
+        Facts.get(tree.factId, function(fact){
+            console.log("TREESGRAPH2.JS: Facts.get callback is this. fact is", fact)
+            const node = {
+                id: tree.treeId,
+                x: tree.x,
+                y: tree.y,
+                label: fact.question + "  " + fact.answer,
+                size: 1,
+                color: '#0FF'
+            }
+            console.log("TREESGRAPH2.JS: node is", node)
+            g.nodes.push(node);
+            const shadowNode = {
+                id: treeId + "_newNode",
+                x: parseInt(tree.x) - 100,
+                y: parseInt(tree.y) + 100,
+                label: 'Create a new Fact',
+                size: 1,
+                color: '#F0F'
+            }
+            console.log("TREESGRAPH2.js nodess is ", g.nodes)
+            g.nodes.push(shadowNode)
+            g.edges.push({
+                id: node.id + "___" + shadowNode.id,
+                source: node.id,
+                target: shadowNode.id,
+                size: 1,
+                color: '#F0F'
+            })
+            console.log("TREESGRAPH2.js nodess is ", g.nodes)
+            sigma.renderers.def = sigma.renderers.canvas
+            s = new sigma({
+                graph: g,
+                container: 'graph-container'
+            });
+            var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+            s.refresh();
+
+        })
+    })
+
+}
 
