93696167bf2b3f13d27a2981105ad345605900cd
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-16 12:58:47 -0700

    Confidence measurement buttons

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const Config = {
-    offlineMode: true // for when I'm trying to code/develop on a train/plane or some place without wifi
+    offlineMode: false // for when I'm trying to code/develop on a train/plane or some place without wifi
 }
