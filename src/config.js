6cbe31ad466c38622639bd49d61c6c49783011f1
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-16 21:10:07 -0700

    version: 2

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const Config = {
-    offlineMode: false // for when I'm trying to code/develop on a train/plane or some place without wifi
+    offlineMode: false, // for when I'm trying to code/develop on a train/plane or some place without wifi
+    version: 2
 }
