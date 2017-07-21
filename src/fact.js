3f825780ce08d8af76e999e152bb312f42264f7f
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-20 19:07:43 -0700

    addTree function on fact

--- src/fact.js
+++ src/fact.js
@@ -4,5 +4,15 @@ export class Fact {
     this.question = question;
     this.answer = answer;
     this.id = md5(JSON.stringify({question: question, answer: answer}));
+    this.trees = {}
+  }
+  addTree(treeId){
+    this.trees[treeId] = true
+    var trees = {}
+    trees[treeId] = true
+    var updates = {
+      trees
+    }
+    firebase.database().ref('facts/' +this.id).update(updates)
   }
 }
