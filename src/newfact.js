d9d3f516c766870dc3570f06e98194b5911cf895
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-20 19:22:57 -0700

    newFact method simplification

--- src/newfact.js
+++ src/newfact.js
@@ -1,14 +1,6 @@
 import {Fact} from './fact.js'
 import getFirebase from './firebaseService.js'
-import {Config} from './config.js'
-import {Globals} from './globals.js'
 //TODO Replace a lot of this with the Trees.js or Tree.js ORM
-function createAndWriteFactFromQA(question,answer){
-    var fact = new Fact(question, answer);
-    console.log('the fact just created is', fact)
-    writeNewFact(fact);
-    return fact;
-}
 function writeNewFact(fact) {
     console.log('THE FACT BEEING CREATED IN NEWFACT.JS is ', fact)
     const firebase = getFirebase();
@@ -16,7 +8,9 @@ function writeNewFact(fact) {
     updates['/facts/' + fact.id] = fact;
     firebase.database().ref().update(updates);
 }
-export function newFact(question, answer){
-    var fact = createAndWriteFactFromQA(question,answer)
+export function newFact(question, answer, treeId){
+    var fact = new Fact(question, answer, treeId)
+    console.log('the fact just created is', fact)
+    writeNewFact(fact)
     return fact;
 }
