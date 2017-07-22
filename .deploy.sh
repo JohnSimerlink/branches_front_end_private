49549641554c323116ea444fddc9b04e368cdbb9
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-22 16:21:57 -0700

    deploy sript edit

--- .deploy.sh
+++ .deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ -f .deploy-configured ]; then
-    git push production master
-else
+if [ ! -f .deploy-configured ]; then
     sh .deploy-config.sh
-    git push production master
 fi
+
+echo "Deploy configuration detected. Building, then committing build, then pushing to production"
+datetime=$(date '+%d/%m/%Y %H:%M:%S');
+npm run build
+git add dist
+git commit -m "Build and Deploy: $datetime"
+git push origin master
+git push production master
+
+
 echo "Deploy script finished! :D :D :D"
