cc6cc8689ea641058a98a0f5ae70921a974f34cb
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 09:34:37 -0700

    partial conversion to angular

--- app/core/bootstrap.js
+++ app/core/bootstrap.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import './components'
 import './objects'
 import './utils'
+import angular from 'angular'
 
 Vue.use(VueFire)
 
+angular.element(document.querySelector('#bootstrap2')).ready( () => {
+    angular.bootstrap(document, ['app'], {
+        strictDi: true
+    });
+});
 new Vue({el: '#bootstrap', render: h => h(App) })
