4c8cdc9b7ebfe09c0835592ef56794ee17d3fd70
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-24 22:45:33 -0700

    newTree separate file

--- app/components/treesGraph.js
+++ app/components/treesGraph.js
@@ -2,10 +2,9 @@ import {Trees} from '../objects/trees.js'
 import {Tree} from '../objects/tree.js'
 import {Facts} from '../objects/facts.js'
 import {Globals} from '../core/globals.js'
-import {toggleVisibility} from "../core/utils"
 import './treeController'
+import './newTreeController'
 import '../core/login.js'
-console.log("treesGraph loaded")
 var initialized = false;
 var s,
     g = {
@@ -47,7 +46,6 @@ var toolTipsConfig = {
     }
 };
 
-
 export function removeTreeFromGraph(treeId){
     s.graph.dropNode(treeId)
     s.graph.dropNode(treeId + newChildTreeSuffix)
@@ -59,7 +57,6 @@ export function removeTreeFromGraph(treeId){
     })
 }
 
-var numTreesLoaded = 0;
 loadTreeAndSubTrees(1).then( val => {initSigma();}/*.then(initSigma)*/)
 // Instantiate sigma:
 function createTreeNodeFromTreeAndFact(tree, fact){
@@ -80,10 +77,13 @@ function createTreeNodeFromTreeAndFact(tree, fact){
 function getLabelFromFact(fact) {
     return fact.question
 }
+function createEdgeId(nodeOneId, nodeTwoId){
+    return nodeOneId + "__" + nodeTwoId
+}
 function connectTreeToParent(tree, g){
     if (tree.parentId) {
         const edge = {
-            id: tree.parentId + "__" + tree.id,
+            id: createEdgeId(tree.parentId, tree.id),
             source: tree.parentId,
             target: tree.id,
             size: 1,
@@ -95,7 +95,7 @@ function connectTreeToParent(tree, g){
 function onGetFact(tree,fact){
     const node = createTreeNodeFromTreeAndFact(tree,fact)
     g.nodes.push(node);
-    addShadowNodeToTree(node)
+    addNewChildTreeToTree(node)
     tree.parentId && connectTreeToParent(tree,g)
     return fact.id
 }
@@ -119,16 +119,15 @@ function onGetTree(tree) {
 //returns a promise whose resolved value will be a stringified representation of the tree's fact and subtrees
 function loadTreeAndSubTrees(treeId){
     //todo: load nodes concurrently, without waiting to connect the nodes or add the fact's informations/labels to the nodes
-    numTreesLoaded++;
     return Trees.get(treeId)
         .then(onGetTree)
         .catch( err => console.error('trees get err is', err))
 }
 
-function addShadowNodeToTree(tree){
+function addNewChildTreeToTree(tree){
     if (tree.children) {
     }
-    const shadowNode = {
+    const newChildTree = {
         id: tree.id + newChildTreeSuffix, //"_newChildTree",
         parentId: tree.id,
         x: parseInt(tree.x) + newNodeXOffset,
@@ -139,28 +138,22 @@ function addShadowNodeToTree(tree){
         type: 'newChildTree'
     }
     const shadowEdge = {
-        id: tree.id + "__" + shadowNode.id,
+        id: createEdgeId(tree.id, newChildTree.id),
         source: tree.id,
-        target: shadowNode.id,
+        target: newChildTree.id,
         size: 1,
         color: Globals.newColor
     };
     if (!initialized) {
-        g.nodes.push(shadowNode)
+        g.nodes.push(newChildTree)
         g.edges.push(shadowEdge)
     } else {
-       s.graph.addNode(shadowNode)
+       s.graph.addNode(newChildTree)
        s.graph.addEdge(shadowEdge)
     }
     if (initialized){
         s.refresh()
     }
-}
-function calculateNewChildNodeCoordinates(treeId){
-    return Trees.get(treeId)
-        .then( tree => {
-
-        })
 }
 function initSigma(){
     if (!initialized){
@@ -174,7 +167,7 @@ function initSigma(){
         s.refresh();
 
         s.bind('click', printNodeInfo )
-        s.bind('outNode', updateTreePosition);
+        s.bind('outNode', updateTreePosition); // after dragging a node, a user's mouse will eventually leave the node, and we need to update the node's position on the graph
         s.bind('overNode', hoverOverNode)
         initialized = true;
     }
@@ -186,55 +179,26 @@ function printNodeInfo(e){
 }
 function hoverOverNode(e){
     var node = e.data.node
-    console.log('tooltips in hoverOverNode is', window.tooltips)
-
-    // Instanciate the tooltips plugin with a Mustache renderer for node tooltips:
-
-    // Manually open a tooltip on a node:
-    var prefix = s.renderers[0].camera.prefix;
-    console.log('prefix is', prefix)
-    tooltips.open(node, toolTipsConfig.node[0], node["renderer1:x"] /*n[prefix + 'x']*/, node["renderer1:y"] /*n[prefix + 'y']*/);
+    tooltips.open(node, toolTipsConfig.node[0], node["renderer1:x"], node["renderer1:y"]);
 }
 function updateTreePosition(e){
-    let x = e.data.node.x
-    let y = e.data.node.y
+    let newX = e.data.node.x
+    let newY = e.data.node.y
     let treeId = e.data.node.id;
 
     if (!s.graph.nodes().find(node => node.id == treeId && node.type === 'tree')){
         return; //node isn't an actual node in the db - its like a shadow node or helper node
     }
+    const MINIMUM_DISTANCE_TO_UPDATE_COORDINATES = 20
     Trees.get(treeId).then( tree => {
-        if (Math.abs(tree.x - x) > 20 ) {
-            tree.set('x', x)
+        if (Math.abs(tree.x - newX) > MINIMUM_DISTANCE_TO_UPDATE_COORDINATES ) {
+            tree.set('x', newX)
         }
-        if (Math.abs(tree.y - y) > 20 ) {
-            tree.set('y', y)
+        if (Math.abs(tree.y - newY) > MINIMUM_DISTANCE_TO_UPDATE_COORDINATES ) {
+            tree.set('y', newY)
         }
         return tree
-    }).then((tree) => {
     })
-}
-function logEvent(e){
-    const MINIMUM_DISTANCE_TO_UPDATE = 20
-    let x = e.data.node.x
-    let y = e.data.node.y
-    let treeId = e.data.node.id;
-    Trees.get(treeId).then( tree => {
-        if (Math.abs(tree.x - x) > MINIMUM_DISTANCE_TO_UPDATE ) {
-           tree.set('x', x)
-        }
-        if (Math.abs(tree.y - y) > MINIMUM_DISTANCE_TO_UPDATE ) {
-            tree.set('y', y)
-        }
-    })
-}
-function dragNode(e){
-    console.log('drag Node', e,e.type, e.data.node, e.data.node.label, e.data.captor);
-    let parentId = e.data.node.parentId;
-    if (e.data.node.type == 'tree'){
-
-    }
-    // let parentTreeId = e.data.node.id.substring(0,e.data.node.id.indexOf("_"));
 }
 
 //returns sigma tree node
@@ -272,10 +236,10 @@ export function addTreeToGraph(parentTreeId, fact) {
     }
     s.graph.addEdge(newEdge)
     //4. add shadow node
-    addShadowNodeToTree(newTree)
+    addNewChildTreeToTree(newTree)
     //5. Re add shadow node to parent
     Trees.get(parentTreeId)
-        .then(addShadowNodeToTree);
+        .then(addNewChildTreeToTree);
 
     s.refresh();
     return newTree;
