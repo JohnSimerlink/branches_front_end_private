4e6f4ea0217b1e9631c4c3486cf99c3c713fbe54
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 09:34:37 -0700

    partial conversion to angular

--- app/core/App.vue
+++ app/core/App.vue
@@ -1,26 +1,19 @@
 <style>
-   #app {
-   }
     #facts, #trees-graph {
         flex: 50;
     }
-    .app-plan {
+    .header-plan {
         margin-left: 8px;
         margin-right: 8px;
         display: inline;
     }
-    .pluginLoginButton {
-        display: flex !important;
-        flex-direction: row;
-    }
-
 
 </style>
 <template>
-   <div id="app">
-       <span class="app-version"> Version: {{version}}</span>
-       <span class="app-plan"><a href="https://docs.google.com/presentation/d/101sNSVZnh-olwaRi4hRR5u6KcFKF78LoV5FXYWGlIT4/edit?usp=sharing">The Plan</a></span>
-       <span class="app-hire"><a href="mailto:john@branches-app.com">Work for Branches</a></span>
+   <div id="header">
+       <span class="header-version"> Version: {{version}}</span>
+       <span class="header-plan"><a href="https://docs.google.com/presentation/d/101sNSVZnh-olwaRi4hRR5u6KcFKF78LoV5FXYWGlIT4/edit?usp=sharing">The Plan</a></span>
+       <span class="header-hire"><a href="mailto:john@branches-app.com">Work for Branches</a></span>
        <!--<div class="fb-login-button" data-max-rows="1" data-size="small" data-button-type="login_with" data-show-faces="true" data-auto-logout-link="false" data-use-continue-as="false"></div>-->
        <button class="login-button"  v-on:click="login"> Login via Facebook </button>
        <span class="login-user-name"></span>
