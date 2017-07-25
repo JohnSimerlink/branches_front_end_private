e7f2ec65bd026415112c3fe5bcc7f172da6cc8b9
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 09:57:46 -0700

    refactor login into common function

--- app/core/App.vue
+++ app/core/App.vue
@@ -22,6 +22,7 @@
 <script>
 import {Config} from './config.js'
 import {toggleVisibility} from './utils.js'
+import {login} from './login.js'
 var userObj = {
     loggedIn: false,
     name: ''
@@ -34,33 +35,7 @@ export default {
     }
   },
     methods: {
-      login: function(event) {
-            console.log('login called')
-          var provider = new firebase.auth.FacebookAuthProvider();
-          firebase.auth().signInWithPopup(provider).then(function (result) {
-              // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-              var token = result.credential.accessToken;
-              document.querySelector('.login-user-name').innerHTML = result.user.displayName
-              document.querySelector('.login-button').style.display = 'none'
-              console.log('result', result)
-              Globals.username = result.user.displayName
-              Globals.userId = result.user.uid
-          }).catch(function (error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // The email of the user's account used.
-              var email = error.email;
-              // The firebase.auth.AuthCredential type that was used.
-              var credential = error.credential;
-          });
-      }
-
-    },
-  components: {
-  }
+      login
+    }
 }
 </script>
-
-<style lang="scss">
-</style>
