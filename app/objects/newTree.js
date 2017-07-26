23e81624529d1066571ffe86963251fa542ed50b
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 18:11:36 -0700

    version 5

--- app/objects/newTree.js
+++ app/objects/newTree.js
@@ -3,11 +3,12 @@ const firebase = getFirebase();
 import {addTreeToGraph} from '../components/treesGraph.js'
 import {Trees} from './trees'
 import {Facts} from './facts'
+//todo: change newTree to Trees.create()
 export function newTree(question, answer, parentTreeId){
     var fact = Facts.create({question,answer})
     const tree = addTreeToGraph(parentTreeId, fact)
     //TODO add a new tree to db and UI by dispatching a new Tree REDUX action
-    //TODO: ^^^ and that action should use the Trees/Tree ORMs we have rather than manually using the db api (bc we may want to swap out db)
+    //TODO: ^^^ and that action should use the Trees/Tree ORMs we have rather than manually using the db api (bc we may want to swap out db from firebase to neo4j or couchdb)
     Trees.get(parentTreeId).then(parentTree => {
         parentTree.addChild(tree.id)
     })
