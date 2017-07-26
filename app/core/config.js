d833ee9e165c34bf694b2b40392ab31c99f36ff3
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 18:11:36 -0700

    version 5

--- app/core/config.js
+++ app/core/config.js
@@ -2,5 +2,5 @@ import ENV from './env.js'
 export const Config = {
     env: ENV, // prod || dev
     offlineMode: false, // for when I'm trying to code/develop on a train/plane or some place without wifi
-    version: 4
+    version: 5
 }
