f6d67fa874346ff84819f89c86e16cc12134f780
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-25 19:06:20 -0700

    rename an obj

--- app/objects/trees.js
+++ app/objects/trees.js
@@ -14,16 +14,15 @@ export class Trees {
            throw "Trees.get(treeId) error!. treeId empty"
         }
         return new Promise( function getTreePromise (resolve, reject) {
-            let treeObj;
 
             //trees serves as local cash for trees downloaded from db //TODO: this cache should become obselete when we switch to Couchdb+pouchdb
             if (trees[treeId]){
                 resolve(trees[treeId])
             } else {
                 firebase.database().ref('trees/' + treeId).on("value", function onFirebaseTreeGet(snapshot){
-                    treeObj = snapshot.val()
-                    var tree = new Tree(treeObj)
-                    trees[tree.id] = tree
+                    let treeData = snapshot.val();
+                    var tree = new Tree(treeData)
+                    trees[tree.id] = tree // add to cache
                     resolve(tree)
                 })
             }
