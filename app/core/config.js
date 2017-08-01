dea0c253bb8055cd5dd090bb7a76f059117e22d2
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-31 19:35:00 -0700

    v7

--- app/core/config.js
+++ app/core/config.js
@@ -2,6 +2,6 @@ import ENV from './env.js'
 export const Config = {
     env: ENV, // prod || dev
     offlineMode: false, // for when I'm trying to code/develop on a train/plane or some place without wifi
-    version: 6,
+    version: 7,
     framework: 'vue' // vue || angular1
 }
