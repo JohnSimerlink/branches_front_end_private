f0e2f8afb1ff4ad924ca353f5fddb083dd1f246c
JohnSimerlink <johnsimerlink@gmail.com>
2017-08-02 19:30:12 -0700

    review in X minutes momentjs

--- app/core/bootstrap.js
+++ app/core/bootstrap.js
@@ -1,11 +1,15 @@
 import './components'
 import './objects'
 import './utils'
+import moment from 'moment'
 import Vue from 'vue'
 import HeaderComponent from '../components/header/branchesHeaderComponent'
 import ReviewScheduleComponent from '../components/reviewAlgorithm/reviewScheduleComponent'
 Vue.component('branchesHeader', HeaderComponent)
 Vue.component('reviewSchedule', ReviewScheduleComponent)
+Vue.filter('timeFromNow', utcTimestamp => {
+    return moment(utcTimestamp).fromNow()
+})
 var vm = new Vue({
     el: '#branches-app'
 })
