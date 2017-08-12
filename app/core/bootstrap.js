848006c8d3c5769c2b754e3d99920ab6f7b83eb8
JohnSimerlink <johnsimerlink@gmail.com>
2017-08-12 10:38:18 -0700

    clean up merge conflict

--- app/core/bootstrap.js
+++ app/core/bootstrap.js
@@ -3,23 +3,16 @@ import './objects'
 import './utils'
 import moment from 'moment'
 import Vue from 'vue'
-<<<<<<< HEAD
-import HeaderComponent from '../components/header/branchesHeader'
-import ReviewScheduleComponent from '../components/reviewAlgorithm/reviewSchedule'
-import TreeComponent from '../components/tree/tree'
-import NewTreeComponent from '../components/newTree/newtreecomponent'
-Vue.component('branchesHeader', HeaderComponent)
-Vue.component('reviewSchedule', ReviewScheduleComponent)
-Vue.component('tree', TreeComponent)
-Vue.component('newtree', NewTreeComponent)
-=======
 import Header from '../components/header/branchesHeader'
 import ReviewSchedule from '../components/reviewAlgorithm/reviewSchedule'
+import Tree from '../components/tree/tree'
+import NewTree from '../components/newTree/newtreecomponent'
 import Toolbar from '../components/toolbar/toolbar'
 Vue.component('branchesHeader', Header)
 Vue.component('reviewSchedule', ReviewSchedule)
+Vue.component('tree', Tree)
+Vue.component('newtree', NewTree)
 Vue.component('toolbar', Toolbar)
->>>>>>> cc75b61bfe15cb264d0639bcf4dc0395c7b32021
 Vue.filter('timeFromNow', utcTimestamp => {
     return moment(utcTimestamp).fromNow()
 })
