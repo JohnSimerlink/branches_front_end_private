b61bc6098bcf54f30f10c89f2ce2a14957c487fd
JohnSimerlink <johnsimerlink@gmail.com>
2017-08-12 10:52:53 -0700

    remove a bunch of console log statements)

--- app/components/header/branchesHeader.js
+++ app/components/header/branchesHeader.js
@@ -4,7 +4,6 @@ import {login} from '../../core/login'
 import user from '../../objects/user'
 import Users from '../../objects/users'
 import PubSub from 'pubsub-js'
-//temporary hacky solution for controller
 export default {
     template: require('./branches-header.html'),
     created () {
@@ -12,26 +11,20 @@ export default {
         self.loggedIn = false
         self.user = {}
         self.username = ''
-        // self.numItemsStudied = 0
         self.numItemsMastered = 0
         self.secondsSpentStudying = 1 // init to 1, not 0 to prevent divBy0 Error
         this.items = {}
 
+
         PubSub.subscribe('login', () => {
-            console.log('login detected inside branchesheader')
             self.loggedIn = true
             self.user = user
             self.username = user.fbData.displayName
+            //TODO: get user object through a Vuex or Redux store. rather than calling Users.get every time
             Users.get(user.getId()).then(user => {
                 self.items = user.items
-                console.log('user received in branchesheader component is', user, user.items, user.items.length)
                 self.user = user
-                // self.numItemsStudied = user.items.length
-                // console.log('numItems studied is', self.numItemsStudied)
-                // console.log('user inside of branchesheader component is ', user, user.branchesData.items.length)
-                // self.numItemsStudied = self.user.branchesData.items.length
             })
-            // console.log('branches header loggedIn is now', self.loggedIn)
         })
     },
     data () {
@@ -41,7 +34,6 @@ export default {
             loggedIn: this.loggedIn,
             username: this.username,
             items: this.items
-            // numItemsStudied: this.numItemsStudied
         }
     },
     computed: {
@@ -57,7 +49,6 @@ export default {
                 if (this.items[key].proficiency >= 96) {
                     sum++
                 }
-                console.log('items mastered reduce being called',sum)
                 return sum
 
             }, 0)
--- app/components/reviewAlgorithm/reviewSchedule.js
+++ app/components/reviewAlgorithm/reviewSchedule.js
@@ -13,16 +13,7 @@ export default {
         this.content = {}
         this.items = {}
         PubSub.subscribe('login',() => {
-
-
             self.loggedIn = true
-
-            // console.log('Review schedule component: login detected')
-            // Users.get(user.getId()).then(user => {
-            //     console.log('review schedule component on login detect: on user get', user)
-            //     self.itemReviewTimeMap = user.branchesData.itemReviewTimeMap
-            // })
-            console.log('review schedule component on login detect: user object is', user)
             Users.get(user.getId()).then( user => {
                 self.items = user.items
             })
@@ -31,8 +22,6 @@ export default {
     },
     data () {
         return {
-            // itemReviewTimeMap: this.itemReviewTimeMap,
-            // numItemsToReview: this.itemReviewTimeMap.length,
             num: 5,
             loggedIn: this.loggedIn,
             items: this.items
