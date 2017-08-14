60dcbcef54d37b03f8d6bdada531b6ebfc1af288
JohnSimerlink <johnsimerlink@gmail.com>
2017-08-14 14:59:56 -0700

    fredoka one font

--- app/core/globals.js
+++ app/core/globals.js
@@ -3,10 +3,10 @@ export const Globals = {
     existingColor: 'brown',
     newColor: 'lightgreen',
     colors: {
-        proficiency_4: 'blue',
-        proficiency_3: 'darkgreen',
+        proficiency_4: 'aqua',
+        proficiency_3: 'lawngreen',
         proficiency_2: 'yellow',
-        proficiency_1: 'brown',
+        proficiency_1: 'lightpink',
         proficiency_unknown: 'gray',
     }
 }
