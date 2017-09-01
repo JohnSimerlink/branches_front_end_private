a85dc79922daa88e3362257d6b9e8868e8661129
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-01 14:44:48 -0700

    correctly says when no exercise found

--- app/components/treeReview/treeReviewContainer.js
+++ app/components/treeReview/treeReviewContainer.js
@@ -1,5 +1,6 @@
 export default {
     template: require('./treeReviewContainer.html'),
     props: ['leafId'],
-    //in browser navigate to  http://localhost:8080/#/review/83cbe6ea3fa874449982b645f04d14a1 - for amar
+    //in browser navigate to  http://localhost:8080/#/study/83cbe6ea3fa874449982b645f04d14a1 - for amar
+    //http://localhost:8080/#/study/83cbe6ea3fa874449982b645f04d14a1
 }
