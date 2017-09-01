e47f3f5b2803bb0724e0c36dfe0098aa7d67959d
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-01 10:07:21 -0700

    router working with props for review leaf id

--- app/components/treeReview/treeReviewContainer.js
+++ app/components/treeReview/treeReviewContainer.js
@@ -1,17 +1,5 @@
 export default {
     template: require('./treeReviewContainer.html'),
-    created () {
-        var me = this;
-        this.leafId = '83cbe6ea3fa874449982b645f04d14a1'
-        PubSub.subscribe('goToState.treeReview', (eventName, treeId) => {
-            this.goToTreeReview()
-        })
-    },
-    data () {
-        return {
-            leafId: this.leafId,
-        }
-    },
-    computed : {
-    }
+    props: ['leafId'],
+    //in browser navigate to  http://localhost:8080/#/review/83cbe6ea3fa874449982b645f04d14a1 - for amar
 }
