1034151e337a963febe8242fd27f2000f72c4e13
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-04 17:27:53 -0700

    initial header proficiency pie chart - doesn't work on updating a child proficiency though

--- other_imports/sigma/renderers/canvas/sigma.canvas.nodes.def.js
+++ other_imports/sigma/renderers/canvas/sigma.canvas.nodes.def.js
@@ -1,32 +1,32 @@
+var PROFICIENCIES = {
+    UNKNOWN: 0,
+    ONE: 33,
+    TWO: 66,
+    THREE: 95,
+    FOUR: 100
+}
+
+var Globals = {
+    currentTreeSelected: null,
+    colors: {
+        proficiency_4: 'aqua',
+        proficiency_3: 'lawngreen',
+        proficiency_2: 'yellow',
+        proficiency_1: 'lightpink',
+        proficiency_unknown: 'gray',
+    }
+}
+function proficiencyToColor(proficiency){
+    if (proficiency > PROFICIENCIES.THREE) return Globals.colors.proficiency_4;
+    if (proficiency > PROFICIENCIES.TWO) return Globals.colors.proficiency_3;
+    if (proficiency > PROFICIENCIES.ONE) return Globals.colors.proficiency_2;
+    if (proficiency > PROFICIENCIES.UNKNOWN) return Globals.colors.proficiency_1;
+    return Globals.colors.proficiency_unknown;
+}
 ;(function() {
   'use strict';
 
-    const PROFICIENCIES = {
-        UNKNOWN: 0,
-        ONE: 33,
-        TWO: 66,
-        THREE: 95,
-        FOUR: 100
-    }
 
-     function proficiencyToColor(proficiency){
-        if (proficiency > PROFICIENCIES.THREE) return Globals.colors.proficiency_4;
-        if (proficiency > PROFICIENCIES.TWO) return Globals.colors.proficiency_3;
-        if (proficiency > PROFICIENCIES.ONE) return Globals.colors.proficiency_2;
-        if (proficiency > PROFICIENCIES.UNKNOWN) return Globals.colors.proficiency_1;
-        return Globals.colors.proficiency_unknown;
-    }
-
-    const Globals = {
-        currentTreeSelected: null,
-        colors: {
-            proficiency_4: 'aqua',
-            proficiency_3: 'lawngreen',
-            proficiency_2: 'yellow',
-            proficiency_1: 'lightpink',
-            proficiency_unknown: 'gray',
-        }
-    }
   sigma.utils.pkg('sigma.canvas.nodes');
 
   /**
@@ -38,7 +38,6 @@
    */
   sigma.canvas.nodes.def = function(node, context, settings) {
       if (node.content.type === 'heading') {
-          console.log('node is', node)
           renderHeading(node, context, settings)
           return
       }
@@ -109,45 +108,54 @@
 })();
 
 function renderHeading(node,context,settings){
-    const proficiencyStats = node.proficiencyStats
-    const numLeaves = Object.keys(proficiencyStats).reduce((accum, statKey) => {
-        return accum + proficiencyStats[statKey]
-    },0)
-    const unknownPercentage = proficiencyStats.UNKNOWN / numLeaves
-    const onePercentage = proficiencyStats.ONE / numLeaves
-    const twoPercentage = proficiencyStats.TWO / numLeaves
-    const threePercentage = proficiencyStats.THREE / numLeaves
-    const fourPercentage = proficiencyStats.FOUR / numLeaves
-
-    const unknownColor = proficiencyToColor(PROFICIENCIES.UNKNOWN)
-    const oneColor = proficiencyToColor(PROFICIENCIES.ONE)
-    const twoColor = proficiencyToColor(PROFICIENCIES.TWO)
-    const threeColor = proficiencyToColor(PROFICIENCIES.THREE)
-    const fourColor = proficiencyToColor(PROFICIENCIES.FOUR)
-
+    var prefix = settings('prefix') || '';
     var x = node[prefix + 'x']
     var y = node[prefix + 'y']
     var size = node[prefix + 'size'];
 
-    let startRadians = 0
+    var unknownColor = proficiencyToColor(PROFICIENCIES.UNKNOWN)
+    var oneColor = proficiencyToColor(PROFICIENCIES.ONE)
+    var twoColor = proficiencyToColor(PROFICIENCIES.TWO)
+    var threeColor = proficiencyToColor(PROFICIENCIES.THREE)
+    var fourColor = proficiencyToColor(PROFICIENCIES.FOUR)
+
+    var proficiencyStats = node.proficiencyStats
+
+    if (! (proficiencyStats instanceof Object)){
+
+        drawPieSlice(context,x,y,size, 0, 2 * Math.PI, unknownColor)
+        return
+    }
+
+    var numLeaves = proficiencyStats instanceof Object ? Object.keys(proficiencyStats).reduce((accum, statKey) => {
+        return accum + proficiencyStats[statKey]
+    },0) : 1
+
+    var unknownPercentage = proficiencyStats.UNKNOWN / numLeaves
+    var onePercentage = proficiencyStats.ONE / numLeaves
+    var twoPercentage = proficiencyStats.TWO / numLeaves
+    var threePercentage = proficiencyStats.THREE / numLeaves
+    var fourPercentage = proficiencyStats.FOUR / numLeaves
+
+    let startRadians = - Math.PI / 2 //start at North
     let endRadians = startRadians + percentageToRadians(unknownPercentage) ///check if i can init based off start, with using comma syntax
-    drawPieSlice(context,x,y, startRadians, endRadians, unknownColor)
+    drawPieSlice(context,x,y,size, startRadians, endRadians, unknownColor)
 
     startRadians = endRadians
     endRadians = startRadians + percentageToRadians(onePercentage)
-    drawPieSlice(context,x,y, startRadians, endRadians, oneColor)
+    drawPieSlice(context,x,y,size, startRadians, endRadians, oneColor)
 
     startRadians = endRadians
     endRadians = startRadians + percentageToRadians(twoPercentage)
-    drawPieSlice(context,x,y, startRadians, endRadians, twoColor)
+    drawPieSlice(context,x,y,size, startRadians, endRadians, twoColor)
 
     startRadians = endRadians
     endRadians = startRadians + percentageToRadians(threePercentage)
-    drawPieSlice(context,x,y, startRadians, endRadians, threeColor)
+    drawPieSlice(context,x,y,size, startRadians, endRadians, threeColor)
 
     startRadians = endRadians
     endRadians = startRadians + percentageToRadians(fourPercentage)
-    drawPieSlice(context,x,y, startRadians, endRadians, fourColor)
+    drawPieSlice(context,x,y,size, startRadians, endRadians, fourColor)
 }
 function percentageToRadians(percentage){
     return percentage * 2 * Math.PI
