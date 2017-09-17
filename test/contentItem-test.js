def89a15e777c41a187087eb326026c42dff5ffc
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-17 09:49:27 -0700

    all unit tests passing

--- test/contentItem-test.js
+++ test/contentItem-test.js
@@ -20,37 +20,39 @@ describe('Get Breadcrumbs', () => {
     // })
 })
 describe('Breadcrumb', () => {
-    it('should correctly get all the breadcrumb sections for Amar Node in branches-dev 8/31 database', () => {
+    it('should correctly get all the breadcrumb sections for Amar Node in branches-dev 9/17 database', () => {
         let everydayWordsTreeNode = {}
         return Trees.get(DATA_KEYS.TREE_IDS.AMAR).then(tree => {
             return ContentItems.get(tree.contentId).then(contentItem => {
                 const breadcrumbs = contentItem.getLastNBreadcrumbsString(4)
                 //Everything > Spanish > Vocab > Everyday Words
-                expect(breadcrumbs).to.equal("Everything > Spanish Vocab > Everyday Words > amar:to love")
+                expect(breadcrumbs).to.equal("Vocab > Nouns > Everyday Words > amar:to love")
             })
         })
     })
-    it('should correctly get all the breadcrumb sections as an array of objects for Amar Node in branches-dev 8/31 database', () => {
+    it('should correctly get all the breadcrumb sections as an array of objects for Amar Node in branches-dev 9/17 database', () => {
         let everydayWordsTreeNode = {}
         return Trees.get(DATA_KEYS.TREE_IDS.AMAR).then(tree => {
             return ContentItems.get(tree.contentId).then(contentItem => {
                 const breadcrumbs = contentItem.getBreadcrumbsObjArray()
                 //Everything > Spanish > Vocab > Everyday Words
-                expect(breadcrumbs.length).to.equal(4)//"Everything > Spanish Vocab > Everyday Words > amar:to love")
-                expect(breadcrumbs[0]).to.equal("Everything")
-                expect(breadcrumbs[1]).to.equal("Spanish Vocab")
-                expect(breadcrumbs[2]).to.equal("Everyday Words")
-                expect(breadcrumbs[3]).to.equal("amar:to love")
+                expect(breadcrumbs.length).to.equal(5)//"Everything > Spanish > Vocab > Everyday Words > amar:to love")
+                expect(breadcrumbs[0].text).to.equal("Spanish")
+                expect(breadcrumbs[1].text).to.equal("Vocab")
+                expect(breadcrumbs[2].text).to.equal("Nouns")
+                expect(breadcrumbs[3].text).to.equal("Everyday Words")
+                expect(breadcrumbs[4].text).to.equal("amar:to love")
             })
         })
     })
-    it('should correctly get all the breadcrumb sections for 1st Person Plural Node in branches-dev 8/31 database', () => {
+    it('should correctly get all the breadcrumb sections for 1st Person Plural Node in branches-dev 9/17 database', () => {
         let everydayWordsTreeNode = {}
         return Trees.get(DATA_KEYS.TREE_IDS.FIRST_PERSON_PLURAL).then(tree => {
             return ContentItems.get(tree.contentId).then(contentItem => {
                 const breadcrumbs = contentItem.getLastNBreadcrumbsString(4)
                 //Everything > Spanish > Vocab > Everyday Words
-                expect(breadcrumbs).to.equal("Indicative Mood > Present Tense > -ar verbs > 1st Person Singular")
+                expect(breadcrumbs.length).to.equal("Present Tense > -ar verbs > Spanish To English > 1st Person Plural".length)
+                expect(breadcrumbs).to.equal("Present Tense > -ar verbs > Spanish to English > 1st Person Plural")
             })
         })
     })
