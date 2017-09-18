7c126a1bb4f6255a8ebb3e994d3c33f994669dc1
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-17 17:10:21 -0700

    better calculateTime algorithm

--- app/components/proficiencyEnum.js
+++ app/components/proficiencyEnum.js
@@ -1,10 +1,10 @@
 import {Globals} from '../core/globals.js'
 export const PROFICIENCIES = {
     UNKNOWN: 0,
-    ONE: 1,
-    TWO: 33,
-    THREE: 67,
-    FOUR: 99,// DON"T make this 100. bc 100/100 is 1. and log of 1 is 0. and n/0 is undefined, which is what was happening in our math.
+    ONE: 12.5,
+    TWO: 37.5,
+    THREE: 62.5,
+    FOUR: 87.5,// DON"T make this 100. bc 100/100 is 1. and log of 1 is 0. and n/0 is undefined, which is what was happening in our math.
 }
 
 export function proficiencyToColor(proficiency){
