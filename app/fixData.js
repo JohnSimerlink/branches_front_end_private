aa938e2eea4de6729150b1a7b8812c15d351cef5
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-19 11:34:07 -0700

    calculate num overdue aggregation works now

--- app/fixData.js
+++ app/fixData.js
@@ -21,4 +21,6 @@ export async function clearInteractionsForHeadings(){
         })
     )
 }
-window.fixInteractionData = fixInteractionData
+if (typeof window !== 'undefined'){
+    window.fixInteractionData = fixInteractionData
+}
