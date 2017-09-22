73ed6f9562eaf643e35cf64996ae708765541e17
JohnSimerlink <johnsimerlink@gmail.com>
2017-09-22 09:55:00 -0700

    snack working again - but no color

--- app/message.js
+++ app/message.js
@@ -1,8 +1,22 @@
-import Snack from 'snackjs'
-export default function message(message) {
+import Snack from 'snack.js'
+if (typeof window !== 'undefined'){
+    window.Snack = Snack
+}
+export default function message(msg) {
+    //
+    // var snack = Snack.make("<your_message>", { /*<your_options>*/ });
+    // snack.show();
+
+//     console.log('Snack constructor is', Snack)
+//     var snack = new Snack();
+// snack.message ("Luke, I'm your father." + msg)
+//      .settings({ duration: Snack.LONG, hideOnClick: true })
+//      .show();
+
+
     var snack = new Snack({
         domParent: document.querySelector('body')
     });
     // show a snack for 4s
-    snack.show(message, 2000);
+    snack.show(msg, 2000);
 }
