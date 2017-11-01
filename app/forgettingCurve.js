de7af2b8a8522880eec465b1566b448e21b832c9
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-01 13:51:10 -0700

    another file converted to typescript

--- app/forgettingCurve.js
+++ app/forgettingCurve.js
@@ -1,87 +1,102 @@
-import {PROFICIENCIES} from "./components/proficiencyEnum.ts";
-
-export const e = 2.7182828
-export const criticalRecall = 1 - 1/e
-//memory strength in decibels - https://docs.google.com/spreadsheets/d/15O87qEZU_t69GrePtRHLTKnmqPUeYeDq0zzGIgRljJs/edit#gid=106595709
-export function calculateStrength(R,t){
-    const proficiencyAsDecimal = R /100
-    const logProficiency = Math.log(proficiencyAsDecimal)
-    const ebbinghaus = -1 * t / logProficiency
-    const dbE = 10 * Math.log10(ebbinghaus)
-    console.log(R, t, " -> ", dbE," dbE")
-    return dbE > 0 ? dbE: 0
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var proficiencyEnum_1 = require("./components/proficiencyEnum");
+exports.e = 2.7182828;
+exports.criticalRecall = 1 - 1 / exports.e;
+/* memory strength in decibels -
+ https://docs.google.com/spreadsheets/d/15O87qEZU_t69GrePtRHLTKnmqPUeYeDq0zzGIgRljJs/edit#gid=106595709
+ */
+function calculateStrength(R, t) {
+    var proficiencyAsDecimal = R / 100;
+    var logProficiency = Math.log(proficiencyAsDecimal);
+    var ebbinghaus = -1 * t / logProficiency;
+    var dbE = 10 * Math.log10(ebbinghaus);
+    console.log(R, t, ' -> ', dbE, ' dbE');
+    return dbE > 0 ? dbE : 0;
 }
-//Se = previous estimated strength
-//R = proficiency from 0 to 100
-//t equals time since previous interaction
-export function calculateSecondsTilCriticalReviewTime(strength){
-    return calculateTime(strength, criticalRecall)
+exports.calculateStrength = calculateStrength;
+// Se = previous estimated strength
+// R = proficiency from 0 to 100
+// t equals time since previous interaction
+function calculateSecondsTilCriticalReviewTime(strength) {
+    return calculateTime(strength, exports.criticalRecall);
 }
+exports.calculateSecondsTilCriticalReviewTime = calculateSecondsTilCriticalReviewTime;
 // function measureInitialPreviousInteractionStrength
-export function measurePreviousStrength(estimatedPreviousStrength, R,t){
-
-    const proficiencyAsDecimal = R / 100
-    const logProficiency = Math.log(proficiencyAsDecimal)
-    const ebbinghaus = -1 * t / logProficiency
-    let measuredPreviousStrength = 10 * Math.log10(ebbinghaus)
-    measuredPreviousStrength = measuredPreviousStrength > 0 ? measuredPreviousStrength: 0
-
-    //if proficiency is greater than/equal to 99 or less than/equal to 1, we have a wide range of possibilities for measured strength values - see this google sheet - https://docs.google.com/spreadsheets/d/15O87qEZU_t69GrePtRHLTKnmqPUeYeDq0zzGIgRljJs/edit#gid=2051263794
-    //therefore just use the previous estimated value, because we can't really measure the actual value
-    //if proficiency is less than 1, and its been a really long time since the user last saw the fact, our measure strength formula above can unintentionally think the user's strength value was much higher than it possible could have been, since we are only recording the value as 1% and not the actual .01% or whatever it actually is
-    if (doubleLessThanOrEqualTo(R, PROFICIENCIES.ONE)/* for double comparison */ && measuredPreviousStrength > estimatedPreviousStrength ){
-        measuredPreviousStrength = estimatedPreviousStrength
-    } else {
+function measurePreviousStrength(estimatedPreviousStrength, R, t) {
+    var proficiencyAsDecimal = R / 100;
+    var logProficiency = Math.log(proficiencyAsDecimal);
+    var ebbinghaus = -1 * t / logProficiency;
+    var measuredPreviousStrength = 10 * Math.log10(ebbinghaus);
+    measuredPreviousStrength = measuredPreviousStrength > 0 ? measuredPreviousStrength : 0;
+    /* if proficiency is greater than/equal to 99 or less than/equal to 1,
+     we have a wide range of possibilities for measured strength values  */
+    // therefore just use the previous estimated value, because we can't really measure the actual value
+    /* if proficiency is less than 1, and its been a really long time
+    since the user last saw the fact,
+    our measure strength formula above can unintentionally think
+     the user's strength value was much higher than it possible
+      could have been,
+      since we are only recording the value as 1% and not the
+      actual .01% or whatever it actually is
+     */
+    if (doubleLessThanOrEqualTo(R, proficiencyEnum_1.PROFICIENCIES.ONE) && measuredPreviousStrength > estimatedPreviousStrength) {
+        measuredPreviousStrength = estimatedPreviousStrength;
     }
-
-    //if proficiency is greater than 99, and its been a really short time since the user last saw the fact, our measure strength formula above can unintentionally think the user's strength value was much lower than it possible could have been, since we are only recording the value as 99% and not the actually 99.99% or whatever it actually is
-    if (doubleGreaterThanOrEqualTo(R,PROFICIENCIES.FOUR) /* for double comparison */ && measuredPreviousStrength < estimatedPreviousStrength ){
-        measuredPreviousStrength = estimatedPreviousStrength
-    } else {
+    /* if proficiency is greater than 99,
+    and its been a really short time since the user last saw the fact,
+     our measure strength formula above can unintentionally think
+      the user's strength value was much lower than it possible could have been,
+      since we are only recording the value as 99% and not the actually 99.99% or whatever it actually is
+      */
+    if (doubleGreaterThanOrEqualTo(R, proficiencyEnum_1.PROFICIENCIES.FOUR) && measuredPreviousStrength < estimatedPreviousStrength) {
+        measuredPreviousStrength = estimatedPreviousStrength;
     }
-    return measuredPreviousStrength
+    return measuredPreviousStrength;
 }
-
-function doubleLessThanOrEqualTo(doubleOne, doubleTwo){
-    return doubleOne <= (doubleTwo + .01)
+exports.measurePreviousStrength = measurePreviousStrength;
+function doubleLessThanOrEqualTo(doubleOne, doubleTwo) {
+    return doubleOne <= (doubleTwo + .01);
 }
-function doubleGreaterThanOrEqualTo(doubleOne, doubleTwo){
-    return doubleOne >= (doubleTwo - .01)
+function doubleGreaterThanOrEqualTo(doubleOne, doubleTwo) {
+    return doubleOne >= (doubleTwo - .01);
 }
-
-//calculate percent change of recall (e.g. proficiency)
-//returns as a num in range [0,1]
-export function calculateRecall(S, t){
-    return Math.pow(e, -1 * t / decibelsToEbbinghaus(S))
+// calculate percent change of recall (e.g. proficiency)
+// returns as a num in range [0,1]
+function calculateRecall(S, t) {
+    return Math.pow(exports.e, -1 * t / decibelsToEbbinghaus(S));
 }
-
-function decibelsToEbbinghaus(dbE){
-    return Math.pow(10, dbE / 10)
+exports.calculateRecall = calculateRecall;
+function decibelsToEbbinghaus(dbE) {
+    return Math.pow(10, dbE / 10);
 }
-//R input is in [0,1]
-export function calculateTime(S, R){
-    return -1 * decibelsToEbbinghaus(S) * Math.log(R)
+// R input is in [0,1]
+function calculateTime(S, R) {
+    return -1 * decibelsToEbbinghaus(S) * Math.log(R);
 }
-
+exports.calculateTime = calculateTime;
 // current proficiency in [0, 100]
-export function estimateCurrentStrength(previousInteractionStrengthDecibels, currentProficiency, secondsSinceLastInteraction){
-    let newInteractionStrengthDecibels
-    const t = secondsSinceLastInteraction
-    if (currentProficiency <= PROFICIENCIES.ONE){
-        const t1percent = calculateTime(previousInteractionStrengthDecibels, currentProficiency / 100)
-        if (t >= t1percent){
-            newInteractionStrengthDecibels= previousInteractionStrengthDecibels * t1percent / t
-        } else {
-            newInteractionStrengthDecibels = previousInteractionStrengthDecibels
+function estimateCurrentStrength(previousInteractionStrengthDecibels, currentProficiency, secondsSinceLastInteraction) {
+    var newInteractionStrengthDecibels;
+    var t = secondsSinceLastInteraction;
+    if (currentProficiency <= proficiencyEnum_1.PROFICIENCIES.ONE) {
+        var t1percent = calculateTime(previousInteractionStrengthDecibels, currentProficiency / 100);
+        if (t >= t1percent) {
+            newInteractionStrengthDecibels = previousInteractionStrengthDecibels * t1percent / t;
         }
-    } else {
-        currentProficiency = currentProficiency / 100
-        const Bt = 1 - calculateRecall(previousInteractionStrengthDecibels,t)
-        const Bp = (e * currentProficiency - 1) / (e - 1)
-        const Bc = 10 * (e - 1)
-        const deltaStrength = Bt * Bp * Bc
-        newInteractionStrengthDecibels = previousInteractionStrengthDecibels + deltaStrength
+        else {
+            newInteractionStrengthDecibels = previousInteractionStrengthDecibels;
+        }
     }
-    newInteractionStrengthDecibels = newInteractionStrengthDecibels < 10 ? 10: newInteractionStrengthDecibels
-    return newInteractionStrengthDecibels
+    else {
+        currentProficiency = currentProficiency / 100;
+        var Bt = 1 - calculateRecall(previousInteractionStrengthDecibels, t);
+        var Bp = (exports.e * currentProficiency - 1) / (exports.e - 1);
+        var Bc = 10 * (exports.e - 1);
+        var deltaStrength = Bt * Bp * Bc;
+        newInteractionStrengthDecibels = previousInteractionStrengthDecibels + deltaStrength;
+    }
+    newInteractionStrengthDecibels = newInteractionStrengthDecibels < 10 ? 10 : newInteractionStrengthDecibels;
+    return newInteractionStrengthDecibels;
 }
+exports.estimateCurrentStrength = estimateCurrentStrength;
---
+++ test/review-test.ts
@@ -0,0 +1,89 @@
+import {expect} from 'chai'
+import * as curve from '../app/forgettingCurve'
+describe('measuringPreviousStrength', () => {
+        it(`should return estimated previous strength if proficiency >= PROFICIENCIES.FOUR
+        and time since last review was really short`, () => {
+            try {
+                expect(curve.measurePreviousStrength(40, 99, 2)).to.equal(40)
+                expect(curve.measurePreviousStrength(40, 99.9, 2)).to.equal(40)
+            } catch (err) {
+                console.error('there was an error', err)
+            }
+        })
+        it(`should return estimated previous strength if proficiency <= 1'
+            ' and time since last review was really long`, () => {
+            try {
+                expect(curve.measurePreviousStrength(40, 1, 9999999)).to.equal(40)
+                expect(curve.measurePreviousStrength(40, .1, 9999999)).to.equal(40)
+            } catch (err) {
+                console.error('there was an error', err)
+            }
+            /* if you were to plug in 1% in our formula for 9999999 seconds
+             you would normally get 63 dB,
+              which doesn't make sense given the previous estimated decibel strength of 40 */
+        })
+        it('should return 10 dbE for 1/e proficiency and 10 seconds', () => {
+            try {
+                expect(curve.measurePreviousStrength(39, 100 * 1 / curve.e, 10)).to.closeTo(10, .1)
+            } catch (err) {
+                console.error('there was an error', err)
+            }
+        })
+        it('should return 50 dbE for 75% proficiency and 8 hours', () => {
+            try {
+                expect(curve.measurePreviousStrength(39, 100 * .75, 8 * 60 * 60)).to.closeTo(50, .1)
+            } catch ( err){
+                console.error(err)
+            }
+        })
+    })
+describe('estimateCurrentStrength', () => {
+        it('should return previous strength for 1/e proficiency', () => {
+            try {
+                expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 8 * 60 * 60)).to.closeTo(39, .1)
+            } catch ( err){
+                console.error('Err is', err)
+            }
+        })
+        it('should return previous strength for 0 seconds since last review', () => {
+            try {
+                expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 0)).to.closeTo(39, .1)
+            } catch ( err){
+                console.error('Err is', err)
+            }
+        })
+        it('should return + 10(e-1) dB for 100% proficiency and longgg time since last review', () => {
+            try {
+                const originalStrength = 39
+                const newExpectedStrength = originalStrength + 10 * (curve.e - 1)
+                expect(curve.estimateCurrentStrength(39, 99.99999, 9999999999)).to.closeTo(newExpectedStrength, .1)
+            } catch ( err){
+                console.error('Err is', err)
+            }
+        })
+        it('should return + 10(1 - 1/e) dB for 100% proficiency at Tc', () => {
+            try {
+                const originalStrength = 39
+                const newExpectedStrength = originalStrength + 10 * ( 1 - 1 / curve.e)
+                const Tc = curve.calculateTime(originalStrength, curve.criticalRecall)
+
+                expect(curve.estimateCurrentStrength(39, 99.99999, Tc)).to.closeTo(newExpectedStrength, .1)
+            } catch ( err){
+                console.error('Err is', err)
+            }
+        })
+        it('should return + 4.4dB for 99% proficiency at 40 minutes past a 39 strength memory ', () => {
+            try {
+                const originalStrength = 39
+                const proficiency = 99
+                const timeSincePreviousInteraction = 40 * 60
+
+                const newStrength = originalStrength + 4.4
+
+                expect(curve.estimateCurrentStrength(originalStrength, proficiency, timeSincePreviousInteraction)).to.closeTo(newStrength, .1)
+            } catch ( err){
+                console.error('Err is', err)
+            }
+        })
+    })
+
