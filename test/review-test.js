de450a710c6e7fd5b8f26031a45c8c67322aaf98
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-01 14:23:47 -0700

    clean up review test

--- test/review-test.js
+++ test/review-test.js
@@ -3,92 +3,48 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var chai_1 = require("chai");
 var curve = require("../app/forgettingCurve");
 describe('measuringPreviousStrength', function () {
-    it("should return estimated previous strength if proficiency >= PROFICIENCIES.FOUR\n        and time since last review was really short", function () {
-        try {
-            chai_1.expect(curve.measurePreviousStrength(40, 99, 2)).to.equal(40);
-            chai_1.expect(curve.measurePreviousStrength(40, 99.9, 2)).to.equal(40);
-        }
-        catch (err) {
-            console.error('there was an error', err);
-        }
+    it("should return estimated previous strength if proficiency >= PROFICIENCIES.FOUR\n    and time since last review was really short", function () {
+        chai_1.expect(curve.measurePreviousStrength(40, 99, 2)).to.equal(40);
+        chai_1.expect(curve.measurePreviousStrength(40, 99.9, 2)).to.equal(40);
     });
-    it("should return estimated previous strength if proficiency <= 1'\n            ' and time since last review was really long", function () {
-        try {
-            chai_1.expect(curve.measurePreviousStrength(40, 1, 9999999)).to.equal(40);
-            chai_1.expect(curve.measurePreviousStrength(40, .1, 9999999)).to.equal(40);
-        }
-        catch (err) {
-            console.error('there was an error', err);
-        }
+    it("should return estimated previous strength if proficiency <= 1'\n        ' and time since last review was really long", function () {
+        chai_1.expect(curve.measurePreviousStrength(40, 1, 9999999)).to.equal(40);
+        chai_1.expect(curve.measurePreviousStrength(40, .1, 9999999)).to.equal(40);
         /* if you were to plug in 1% in our formula for 9999999 seconds
          you would normally get 63 dB,
           which doesn't make sense given the previous estimated decibel strength of 40 */
     });
     it('should return 10 dbE for 1/e proficiency and 10 seconds', function () {
-        try {
-            chai_1.expect(curve.measurePreviousStrength(39, 100 * 1 / curve.e, 10)).to.closeTo(10, .1);
-        }
-        catch (err) {
-            console.error('there was an error', err);
-        }
+        chai_1.expect(curve.measurePreviousStrength(39, 100 * 1 / curve.e, 10)).to.closeTo(10, .1);
     });
     it('should return 50 dbE for 75% proficiency and 8 hours', function () {
-        try {
-            chai_1.expect(curve.measurePreviousStrength(39, 100 * .75, 8 * 60 * 60)).to.closeTo(50, .1);
-        }
-        catch (err) {
-            console.error(err);
-        }
+        chai_1.expect(curve.measurePreviousStrength(39, 100 * .75, 8 * 60 * 60)).to.closeTo(50, .1);
     });
 });
 describe('estimateCurrentStrength', function () {
     it('should return previous strength for 1/e proficiency', function () {
-        try {
-            chai_1.expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 8 * 60 * 60)).to.closeTo(39, .1);
-        }
-        catch (err) {
-            console.error('Err is', err);
-        }
+        chai_1.expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 8 * 60 * 60)).to.closeTo(39, .1);
     });
     it('should return previous strength for 0 seconds since last review', function () {
-        try {
-            chai_1.expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 0)).to.closeTo(39, .1);
-        }
-        catch (err) {
-            console.error('Err is', err);
-        }
+        chai_1.expect(curve.estimateCurrentStrength(39, 100 * 1 / curve.e, 0)).to.closeTo(39, .1);
     });
     it('should return + 10(e-1) dB for 100% proficiency and longgg time since last review', function () {
-        try {
-            var originalStrength = 39;
-            var newExpectedStrength = originalStrength + 10 * (curve.e - 1);
-            chai_1.expect(curve.estimateCurrentStrength(39, 99.99999, 9999999999)).to.closeTo(newExpectedStrength, .1);
-        }
-        catch (err) {
-            console.error('Err is', err);
-        }
+        var originalStrength = 39;
+        var newExpectedStrength = originalStrength + 10 * (curve.e - 1);
+        chai_1.expect(curve.estimateCurrentStrength(39, 99.99999, 9999999999)).to.closeTo(newExpectedStrength, .1);
     });
     it('should return + 10(1 - 1/e) dB for 100% proficiency at Tc', function () {
-        try {
-            var originalStrength = 39;
-            var newExpectedStrength = originalStrength + 10 * (1 - 1 / curve.e);
-            var Tc = curve.calculateTime(originalStrength, curve.criticalRecall);
-            chai_1.expect(curve.estimateCurrentStrength(39, 99.99999, Tc)).to.closeTo(newExpectedStrength, .1);
-        }
-        catch (err) {
-            console.error('Err is', err);
-        }
+        var originalStrength = 39;
+        var newExpectedStrength = originalStrength + 10 * (1 - 1 / curve.e);
+        var Tc = curve.calculateTime(originalStrength, curve.criticalRecall);
+        chai_1.expect(curve.estimateCurrentStrength(39, 99.99999, Tc)).to.closeTo(newExpectedStrength, .1);
     });
     it('should return + 4.4dB for 99% proficiency at 40 minutes past a 39 strength memory ', function () {
-        try {
-            var originalStrength = 39;
-            var proficiency = 99;
-            var timeSincePreviousInteraction = 40 * 60;
-            var newStrength = originalStrength + 4.4;
-            chai_1.expect(curve.estimateCurrentStrength(originalStrength, proficiency, timeSincePreviousInteraction)).to.closeTo(newStrength, .1);
-        }
-        catch (err) {
-            console.error('Err is', err);
-        }
+        var originalStrength = 39;
+        var proficiency = 99;
+        var timeSincePreviousInteraction = 40 * 60;
+        var newStrength = originalStrength + 4.4;
+        var estimatedCurrentStrength = curve.estimateCurrentStrength(originalStrength, proficiency, timeSincePreviousInteraction);
+        chai_1.expect(estimatedCurrentStrength).to.closeTo(newStrength, .1);
     });
 });
--- test/uriParser-test.js
+++ test/uriParser-test.js
@@ -1,125 +1,77 @@
-import {convertBreadcrumbListToString, getLastNBreadcrumbsStringFromList} from "../app/objects/contentItem/uriParser.ts";
-import {expect} from 'chai'
-describe('Convert Breadcrumbs List to String', () => {
-    let list, expectedBreadcrumbs
-    list = ['a','b','c']
-    expectedBreadcrumbs = "a > b > c"
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = convertBreadcrumbListToString(list)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-    list = ['Everything','Spanish', 'Vocab', 'amar : to love']
-    expectedBreadcrumbs = "Everything > Spanish > Vocab > amar : to love"
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = convertBreadcrumbListToString(list)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-    list = []
-    expectedBreadcrumbs = ""
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = convertBreadcrumbListToString(list)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-})
-describe('Last N BreadCrumbs String From List', () => {
-    let list, expectedBreadcrumbs, n
-    list = ['a','b','c']
-    expectedBreadcrumbs = "b > c"
-    n = 2
-    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-
-        } catch (err) {
-            console.error("there was an error", err)
-            return false
-        }
-    })
-    list = ['a','b','c']
-    expectedBreadcrumbs = "a > b > c"
-    n = 4
-    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-            return false
-        }
-    })
-
-    list = ['a','b','c']
-    expectedBreadcrumbs = "c"
-    n = 1
-    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-    list = ['a','b','c']
-    expectedBreadcrumbs = ""
-    n =  0
-    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-
-    list = ['Everything','Spanish', 'Vocab', 'amar : to love']
-    expectedBreadcrumbs = "Everything > Spanish > Vocab > amar : to love"
-    n =  4
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-    list = ['Everything','Spanish', 'Vocab', 'amar : to love']
-    expectedBreadcrumbs = "Spanish > Vocab > amar : to love"
-    n =  3
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-
-    list = []
-    expectedBreadcrumbs = ""
-    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, () => {
-        const breadcrumbs = getLastNBreadcrumbsStringFromList(list, n)
-        try {
-            expect(breadcrumbs).to.equal(expectedBreadcrumbs)
-        } catch (err) {
-            console.error("there was an error", err)
-        }
-    })
-})
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var uriParser_1 = require("../app/objects/contentItem/uriParser");
+var chai_1 = require("chai");
+describe('Convert Breadcrumbs List to String', function () {
+    var list;
+    var expectedBreadcrumbs;
+    list = ['a', 'b', 'c'];
+    expectedBreadcrumbs = 'a > b > c';
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.convertBreadcrumbListToString(list);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['Everything', 'Spanish', 'Vocab', 'amar : to love'];
+    expectedBreadcrumbs = 'Everything > Spanish > Vocab > amar : to love';
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.convertBreadcrumbListToString(list);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = [];
+    expectedBreadcrumbs = '';
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.convertBreadcrumbListToString(list);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+});
+describe('Last N BreadCrumbs String From List', function () {
+    var list, expectedBreadcrumbs, n;
+    list = ['a', 'b', 'c'];
+    expectedBreadcrumbs = 'b > c';
+    n = 2;
+    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['a', 'b', 'c'];
+    expectedBreadcrumbs = 'a > b > c';
+    n = 4;
+    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['a', 'b', 'c'];
+    expectedBreadcrumbs = 'c';
+    n = 1;
+    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['a', 'b', 'c'];
+    expectedBreadcrumbs = '';
+    n = 0;
+    it('should convert ' + list.toString() + ', ' + n + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['Everything', 'Spanish', 'Vocab', 'amar : to love'];
+    expectedBreadcrumbs = 'Everything > Spanish > Vocab > amar : to love';
+    n = 4;
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = ['Everything', 'Spanish', 'Vocab', 'amar : to love'];
+    expectedBreadcrumbs = 'Spanish > Vocab > amar : to love';
+    n = 3;
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+    list = [];
+    expectedBreadcrumbs = '';
+    it('should convert ' + list.toString() + ' to ' + expectedBreadcrumbs, function () {
+        var breadcrumbs = uriParser_1.getLastNBreadcrumbsStringFromList(list, n);
+        chai_1.expect(breadcrumbs).to.equal(expectedBreadcrumbs);
+    });
+});
