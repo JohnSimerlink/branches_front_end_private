79496af171cd77dbdac2889f31f448e793ddb9c2
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-01 14:23:47 -0700

    clean up review test

--- test/uri-test.js
+++ test/uri-test.js
@@ -1,10 +1,8 @@
-describe('uri', () => {
-   it('user or program should not be able to create uri longer than 768 bytes', () => {
-       try {
-
-       } catch (err) {
-
-       }
-
-   })
-})
+describe('uri', function () {
+    it('user or program should not be able to create uri longer than 768 bytes', function () {
+        try {
+        }
+        catch (err) {
+        }
+    });
+});
