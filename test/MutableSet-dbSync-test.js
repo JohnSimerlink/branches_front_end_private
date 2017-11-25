49d6da5f90ff5261b08d04107372cf3a53556bb5
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-25 12:52:36 -0800

    Rename and delete some files

--- test/MutableSet-dbSync-test.js
+++
@@ -1,2 +0,0 @@
-describe('MutableSet-dbSync', function () {
-});
--- test/uri-test.js
+++
@@ -1,8 +0,0 @@
-describe('uri', function () {
-    it('user or program should not be able to create uri longer than 768 bytes', function () {
-        try {
-        }
-        catch (err) {
-        }
-    });
-});
