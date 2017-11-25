1cd4feecaade476160ea2033ef802e38b9651706
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-25 12:59:13 -0800

    Move test files to their correct locations

--- app/objects/tree/proficiencyStats.spec.ts
+++ app/objects/tree/proficiencyStats.spec.ts
@@ -2,7 +2,7 @@
 import {expect} from 'chai'
 import {} from 'mocha' // types
 
-import {addObjToProficiencyStats} from '../app/objects/tree/proficiencyStats';
+import {addObjToProficiencyStats} from './proficiencyStats';
 
 describe('addObjToProficiencyStats', () => {
 
