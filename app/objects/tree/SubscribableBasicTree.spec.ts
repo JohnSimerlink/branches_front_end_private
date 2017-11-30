472fefbe4cd0d493388f2c9b31d84a987f265783
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-29 20:21:03 -0800

    Pass tree on update upon update of a descendant property

--- app/objects/tree/SubscribableBasicTree.spec.ts
+++ app/objects/tree/SubscribableBasicTree.spec.ts
@@ -8,6 +8,8 @@ import {IDatedMutation} from '../mutations/IMutation';
 import {ISubscribableMutableStringSet} from '../set/ISubscribableMutableStringSet';
 import {TYPES} from '../types';
 import {SubscribableBasicTree} from './SubscribableBasicTree';
+import {SubscribableMutableId} from '../id/SubscribableMutableId';
+import {SubscribableMutableStringSet} from '../set/SubscribableMutableStringSet';
 
 describe('FirebaseSyncableBasicTree', () => {
     it('constructor should set all the subscribable properties', () => {
@@ -23,9 +25,12 @@ describe('FirebaseSyncableBasicTree', () => {
     })
     it('a mutation in one of the subscribable properties' +
         ' should publish an update of the entire object\'s value', () => {
-        const contentId = myContainer.get<ISubscribableMutableId>(TYPES.ISubscribableMutableId)
-        const parentId = myContainer.get<ISubscribableMutableId>(TYPES.ISubscribableMutableId)
-        const children = myContainer.get<ISubscribableMutableStringSet>(TYPES.ISubscribableMutableStringSet)
+        const contentId = new SubscribableMutableId()
+        /* = myContainer.get<ISubscribableMutableId>(TYPES.ISubscribableMutableId)
+         // TODO: figure out why DI puts in a bad updatesCallback!
+        */
+        const parentId = new SubscribableMutableId()
+        const children = new SubscribableMutableStringSet()
         const TREE_ID = 'efa123'
         const tree = new SubscribableBasicTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
         tree.publishUponDescendantUpdates()
@@ -36,8 +41,9 @@ describe('FirebaseSyncableBasicTree', () => {
         const sampleMutation = myContainer.get<IDatedMutation<IdMutationTypes>>(TYPES.IDatedMutation)
         contentId.addMutation(sampleMutation)
         const newTreeDataValue = tree.val()
+        const calledWith = callback.getCall(0).args[0]
         expect(callback.callCount).to.equal(1)
-        assert(callback.calledWith(newTreeDataValue))
+        expect(calledWith.updates.val).to.deep.equal(newTreeDataValue)
 
     })
 })
