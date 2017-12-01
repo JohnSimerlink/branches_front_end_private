09e85240da2ceba001d41ceb780018cd08e19fb0
JohnSimerlink <johnsimerlink@gmail.com>
2017-11-30 16:14:30 -0800

    Pass SubscribableGlobalTree test

--- app/objects/dataStores/SubscribableGlobalDataStore.spec.ts
+++ app/objects/dataStores/SubscribableGlobalDataStore.spec.ts
@@ -1,6 +1,7 @@
 import {expect} from 'chai'
 import * as sinon from 'sinon'
 import {myContainer} from '../../../inversify.config';
+import {log} from '../../core/log'
 import {IdMutationTypes} from '../id/IdMutationTypes';
 import {SubscribableMutableId} from '../id/SubscribableMutableId';
 import {IDatedMutation} from '../mutations/IMutation';
@@ -13,6 +14,29 @@ import {SubscribableGlobalDataStore} from './SubscribableGlobalDataStore';
 import {ISubscribableTreeDataStore} from './SubscribableTreeDataStore';
 
 describe('SubscribableGlobalDataStore', () => {
+    it(' calling startBroadcasting on GlobalStore, should call onUpdate on each of the component Stores', () => {
+        const contentId = new SubscribableMutableId()
+        const parentId = new SubscribableMutableId()
+        const children = new SubscribableMutableStringSet()
+        const TREE_ID = 'efa123'
+        const tree = new SubscribableBasicTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
+        // const tree = myContainer.get<ISubscribableBasicTree>(TYPES.ISubscribableBasicTree)
+        // <<< TODO: using this dependency injection causes this entire test to fail. WHY?
+        tree.publishUponDescendantUpdates()
+        const treeStore = myContainer.get<ISubscribableTreeDataStore>(TYPES.ISubscribableTreeDataStore)
+        const globalStore = new SubscribableGlobalDataStore(
+            {
+                subscribableTreeDataStore: treeStore,
+                updatesCallbacks: [],
+            }
+        )
+        const treeStoreOnUpdateSpy = sinon.spy(treeStore, 'onUpdate')
+
+        expect(treeStoreOnUpdateSpy.callCount).to.equal(0)
+        globalStore.startBroadcasting()
+        expect(treeStoreOnUpdateSpy.callCount).to.equal(1)
+    })
+
     it('After calling startBroadcasting, globalStore should publish updates'
         + ' when one of its component stores publishes an update', () => {
         const contentId = new SubscribableMutableId()
@@ -51,10 +75,40 @@ describe('SubscribableGlobalDataStore', () => {
         expect(callback2.getCall(0).args[0].type).to.deep.equal(ObjectTypes.TREE)
 
     })
-
+    //
     it('Before calling startBroadcasting, globalStore should NOT publish updates ' +
         ' when one of its component stores publishes an update', () => {
 
+    const contentId = new SubscribableMutableId()
+    const parentId = new SubscribableMutableId()
+    const children = new SubscribableMutableStringSet()
+    const TREE_ID = 'efa123'
+    const tree = new SubscribableBasicTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
+    // const tree = myContainer.get<ISubscribableBasicTree>(TYPES.ISubscribableBasicTree)
+    // <<< TODO: using this dependency injection causes this entire test to fail. WHY?
+    tree.publishUponDescendantUpdates()
+    const treeStore = myContainer.get<ISubscribableTreeDataStore>(TYPES.ISubscribableTreeDataStore)
+    const globalStore = new SubscribableGlobalDataStore(
+        {
+            subscribableTreeDataStore: treeStore,
+            updatesCallbacks: [],
+        }
+    )
+    // log('subscribableGlobalDataStore spec called. global store just created', globalStore)
+    const callback1 = sinon.spy()
+    const callback2 = sinon.spy()
+
+    // log('subscribableGlobalDataStore spec called. about to call onUpdate ', globalStore['onUpdate'])
+    globalStore.onUpdate(callback2)
+    globalStore.onUpdate(callback1)
+    treeStore.addAndSubscribeToItem({id: TREE_ID, item: tree})
+    const sampleMutation = myContainer.get<IDatedMutation<IdMutationTypes>>(TYPES.IDatedMutation)
+    // globalStore.startBroadcasting()
+    tree.addDescendantMutation(TreePropertyNames.parentId, sampleMutation)
+
+    const treeNewVal = tree.val()
+    expect(callback1.callCount).to.equal(0)
+    expect(callback2.callCount).to.equal(0)
     })
 
 })
