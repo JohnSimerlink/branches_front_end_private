4c30de4c5b995e39d1b7785ea27e4f6fcac5c58b
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-01 13:58:42 -0800

    Add requirement such that sigma ui updates can update multiple sigma nodes. e.g. one to many contentId to sigmaId relationship

--- app/objects/sigmaNode/SigmaNodeHandler.spec.ts
+++ app/objects/sigmaNode/SigmaNodeHandler.spec.ts
@@ -3,25 +3,43 @@ import * as sinon from 'sinon'
 import {myContainer} from '../../../inversify.config';
 import {CONTENT_TYPES} from '../contentItem/ContentTypes';
 import {IContentData} from '../contentItem/IContentData';
+import {IContentUserData} from '../contentUserData/IContentUserData';
 import {ObjectDataTypes} from '../dataStores/ObjectTypes';
 import {ITypeAndIdAndValUpdates} from '../interfaces';
+import {ICoordinate} from '../point/IPoint';
+import {PROFICIENCIES} from '../proficiency/proficiencyEnum';
 import {IBasicTreeDataWithoutId} from '../tree/IBasicTreeData';
+import {ITreeUserData} from '../treeUserData/ITreeUserData';
 import {TYPES} from '../types';
 import {ISigmaNode} from './ISigmaNode';
 import {SigmaNodeHandler} from './SigmaNodeHandler';
-import {IContentUserData} from '../contentUserData/IContentUserData';
-import {PROFICIENCIES} from '../proficiency/proficiencyEnum';
-import {ITreeUserData} from '../treeUserData/ITreeUserData';
-import {ICoordinate} from '../point/IPoint';
 
+const TREE_ID = '12334'
+const TREE_ID2 = '1252334'
+const CONTENT_ID = '5982347'
+function getSigmaIdsForContentId(contentId) {
+   switch (contentId) {
+       case CONTENT_ID:
+           return [TREE_ID, TREE_ID2]
+   }
+}
+let sigmaNodes
+let sigmaNode1
+let sigmaNode2
+
+let sigmaNodeHandler
 describe('SigmaNodeHandler', () => {
-    it('A Tree Update should call the correct method on the sigma Node with the correct args', () => {
-        const sigmaNode = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
-        const TREE_ID = '12334'
-        const sigmaNodes = {}
-        sigmaNodes[TREE_ID] = sigmaNode
+    beforeEach('init sigmaNodes', () => {
+        sigmaNode1 = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
+        sigmaNode2 = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
+        sigmaNodes = {}
+        sigmaNodes[TREE_ID] = sigmaNode1
+        sigmaNodes[TREE_ID] = sigmaNode2
 
-        const contentIdSigmaIdMap = {}
+        sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, getSigmaIdsForContentId})
+    })
+    it('A Tree Update should call the correct method on the sigma Node with the correct args', () => {
+
         const newContentId = '4324234'
         const newParentId = '4344324234'
         const newChildren = ['45344324234', 'aabc321', 'abcd43132']
@@ -35,19 +53,14 @@ describe('SigmaNodeHandler', () => {
             type: ObjectDataTypes.TREE_DATA,
             val,
         }
-        const sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, contentIdSigmaIdMap})
-        const sigmaNodeReceiveNewTreeDataSpy = sinon.spy(sigmaNode, 'receiveNewTreeData')
+        const sigmaNode1ReceiveNewTreeDataSpy = sinon.spy(sigmaNode1, 'receiveNewTreeData')
+        const sigmaNode2ReceiveNewTreeDataSpy = sinon.spy(sigmaNode2, 'receiveNewTreeData')
 
         sigmaNodeHandler.handleUpdate(update)
-        expect(sigmaNodeReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode1ReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode2ReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
     })
     it('A Tree Location Update should call the correct method on the sigma Node with the correct args', () => {
-        const sigmaNode = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
-        const TREE_ID = '12334'
-        const sigmaNodes = {}
-        sigmaNodes[TREE_ID] = sigmaNode
-
-        const contentIdSigmaIdMap = {}
         const val: ICoordinate = {
             x: 5,
             y: 9,
@@ -57,19 +70,15 @@ describe('SigmaNodeHandler', () => {
             type: ObjectDataTypes.TREE_LOCATION_DATA,
             val,
         }
-        const sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, contentIdSigmaIdMap})
-        const sigmaNodeReceiveNewTreeLocationDataSpy = sinon.spy(sigmaNode, 'receiveNewTreeLocationData')
+
+        const sigmaNode1ReceiveNewTreeLocationDataSpy = sinon.spy(sigmaNode1, 'receiveNewTreeLocation')
+        const sigmaNode2ReceiveNewTreeLocationDataSpy = sinon.spy(sigmaNode2, 'receiveNewTreeLocation')
 
         sigmaNodeHandler.handleUpdate(update)
-        expect(sigmaNodeReceiveNewTreeLocationDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode1ReceiveNewTreeLocationDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode2ReceiveNewTreeLocationDataSpy.getCall(0).args[0]).to.deep.equal(val)
     })
     it('A Tree User Data Update should call the correct method on the sigma Node with the correct args', () => {
-        const sigmaNode = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
-        const TREE_ID = '12334'
-        const sigmaNodes = {}
-        sigmaNodes[TREE_ID] = sigmaNode
-
-        const contentIdSigmaIdMap = {}
         const val: ITreeUserData = {
             aggregationTimer: 1230,
             proficiencyStats: {
@@ -86,24 +95,14 @@ describe('SigmaNodeHandler', () => {
             type: ObjectDataTypes.TREE_USER_DATA,
             val,
         }
-        const sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, contentIdSigmaIdMap})
-        const sigmaNodeReceiveNewTreeDataSpy = sinon.spy(sigmaNode, 'receiveNewTreeUserData')
+        const sigmaNode1ReceiveNewTreeDataSpy = sinon.spy(sigmaNode1, 'receiveNewTreeUserData')
+        const sigmaNode2ReceiveNewTreeDataSpy = sinon.spy(sigmaNode2, 'receiveNewTreeUserData')
 
         sigmaNodeHandler.handleUpdate(update)
-        expect(sigmaNodeReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode1ReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode2ReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
     })
     it('A Content Update should call the correct method on the sigma Node with the correct args', () => {
-        const sigmaNode = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
-        const TREE_ID = '12334'
-        const CONTENT_ID = '5982347'
-        const sigmaNodes = {}
-        sigmaNodes[TREE_ID] = sigmaNode
-
-        const contentIdSigmaIdMap = {}
-        contentIdSigmaIdMap[CONTENT_ID] = TREE_ID
-        const newContentId = '4324234'
-        const newParentId = '4344324234'
-        const newChildren = ['45344324234', 'aabc321', 'abcd43132']
         const val: IContentData = {
             answer: 'Sacramento',
             question: 'California',
@@ -115,21 +114,14 @@ describe('SigmaNodeHandler', () => {
             type: ObjectDataTypes.CONTENT_DATA,
             val,
         }
-        const sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, contentIdSigmaIdMap})
-        const sigmaNodeReceiveNewTreeDataSpy = sinon.spy(sigmaNode, 'receiveNewContentData')
+        const sigmaNode1ReceiveNewContentDataSpy = sinon.spy(sigmaNode1, 'receiveNewContentData')
+        const sigmaNode2ReceiveNewContentDataSpy = sinon.spy(sigmaNode2, 'receiveNewContentData')
 
         sigmaNodeHandler.handleUpdate(update)
-        expect(sigmaNodeReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode1ReceiveNewContentDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode2ReceiveNewContentDataSpy.getCall(0).args[0]).to.deep.equal(val)
     })
     it('A Content User Update should call the correct method on the sigma Node with the correct args', () => {
-        const sigmaNode = myContainer.get<ISigmaNode>(TYPES.ISigmaNode)
-        const TREE_ID = '12334'
-        const CONTENT_ID = '5982347'
-        const sigmaNodes = {}
-        sigmaNodes[TREE_ID] = sigmaNode
-
-        const contentIdSigmaIdMap = {}
-        contentIdSigmaIdMap[CONTENT_ID] = TREE_ID
         const val: IContentUserData = {
             lastRecordedStrength: 54, // TODO: this mig
             overdue: true,
@@ -142,10 +134,11 @@ describe('SigmaNodeHandler', () => {
             type: ObjectDataTypes.CONTENT_USER_DATA,
             val,
         }
-        const sigmaNodeHandler = new SigmaNodeHandler({sigmaNodes, contentIdSigmaIdMap})
-        const sigmaNodeReceiveNewTreeDataSpy = sinon.spy(sigmaNode, 'receiveNewContentUserData')
+        const sigmaNode1ReceiveNewContentUserDataSpy = sinon.spy(sigmaNode1, 'receiveNewContentUserData')
+        const sigmaNode2ReceiveNewContentUserDataSpy = sinon.spy(sigmaNode2, 'receiveNewContentUserData')
 
         sigmaNodeHandler.handleUpdate(update)
-        expect(sigmaNodeReceiveNewTreeDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode1ReceiveNewContentUserDataSpy.getCall(0).args[0]).to.deep.equal(val)
+        expect(sigmaNode2ReceiveNewContentUserDataSpy.getCall(0).args[0]).to.deep.equal(val)
     })
 })
