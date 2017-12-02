d563496adfc27d2698882ab7077bc9b8e164204a
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-02 13:44:13 -0800

    Rename some properties on App

--- app/core/app.ts
+++ app/core/app.ts
@@ -1,26 +1,29 @@
 import {inject, injectable} from 'inversify';
-import {ISubscribableGlobalDataStore} from '../objects/dataStores/SubscribableGlobalDataStore';
-import {IApp, ISigmaNodeHandlerSubscriber} from '../objects/interfaces';
+import {IApp, IMutableSubscribableGlobalDataStore, ISigmaNodeHandlerSubscriber} from '../objects/interfaces';
 import {TYPES} from '../objects/types';
 @injectable()
 class App implements IApp {
     private canvasUI: ISigmaNodeHandlerSubscriber // TODO: replace with an interface and/or generic
-    private dataChangeEmitter: ISubscribableGlobalDataStore
-    constructor(@inject(TYPES.AppArgs){sigmaNodeHandlerSubscriber, subscribableGlobalDataStore}) {
+    /* TODO: rather than hardcoding all the types of UIs that subscribe to the app,
+     let the app invocation or inversify object graph dynamically choose which UIs should be part of the app
+     */
+    private dataStore: IMutableSubscribableGlobalDataStore
+    constructor(@inject(TYPES.AppArgs){sigmaNodeHandlerSubscriber, dataStore}) {
         this.canvasUI = sigmaNodeHandlerSubscriber
-        this.dataChangeEmitter = subscribableGlobalDataStore
+        this.dataStore = dataStore
     }
     public start() {
-        // this.dataChangeEmitter.loadFromCache() // or // dataChangeEmitter.init() or     something
+        // this.dataStore.loadFromCache() // or // dataStore.init() or     something
         // ^^^ TODO: << figure out how / when / what data we will load
-        this.canvasUI.subscribe(this.dataChangeEmitter)
-        // this.shellUI.subscribe(this.dataChangeEmitter)
-        this.dataChangeEmitter.startBroadcasting()
+        this.canvasUI.subscribe(this.dataStore)
+        // this.shellUI.subscribe(this.dataStore)
+        this.dataStore.startBroadcasting()
     }
 }
 
 @injectable()
 class AppArgs {
     @inject(TYPES.ISigmaNodeHandlerSubscriber) public sigmaNodeHandlerSubscriber
+    @inject(TYPES.IMutableSubscribableGlobalDataStore) public dataStore
 }
 export {App, AppArgs}
