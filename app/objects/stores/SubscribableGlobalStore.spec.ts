40e6024b6a60a0b36632701df2de02fa4defceae
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-05 09:34:11 -0800

    Make Global Store publishing contentUser updates

--- app/objects/stores/SubscribableGlobalStore.spec.ts
+++ app/objects/stores/SubscribableGlobalStore.spec.ts
@@ -4,7 +4,11 @@ import {myContainer} from '../../../inversify.config';
 import {log} from '../../core/log'
 import {SubscribableMutableField} from '../field/SubscribableMutableField';
 import {
-    FieldMutationTypes, IMutableSubscribableTree, IProppedDatedMutation, ISubscribableGlobalStore,
+    ContentUserPropertyMutationTypes, ContentUserPropertyNames,
+    FieldMutationTypes, IIdProppedDatedMutation, IMutableSubscribableContentUser, IMutableSubscribableTree,
+    IProppedDatedMutation,
+    ISubscribableContentUserStore,
+    ISubscribableGlobalStore,
     ISubscribableMutableField,
     ISubscribableMutableStringSet, ISubscribableStore, ISubscribableTreeCore, ISubscribableTreeStore,
     TreePropertyNames
@@ -14,8 +18,11 @@ import {SubscribableMutableStringSet} from '../set/SubscribableMutableStringSet'
 import {MutableSubscribableTree} from '../tree/MutableSubscribableTree';
 import {SubscribableTree} from '../tree/SubscribableTree';
 import {TYPES} from '../types';
+import {SubscribableContentUserStore} from './contentUser/SubscribableContentUserStore';
 import {SubscribableGlobalStore} from './SubscribableGlobalStore';
 import {SubscribableTreeStore} from './tree/SubscribableTreeStore';
+import {PROFICIENCIES} from '../proficiency/proficiencyEnum';
+import {MutableSubscribableContentUser} from '../contentUserData/MutableSubscribableContentUser';
 
 describe('ISubscribableGlobalStore', () => {
     it(' calling startPublishing on GlobalStore, should call onUpdate on each of the component Stores', () => {
@@ -35,21 +42,29 @@ describe('ISubscribableGlobalStore', () => {
             store: {},
             updatesCallbacks: []
         })
+        const contentUserStore: ISubscribableContentUserStore = new SubscribableContentUserStore({
+            store: {},
+            updatesCallbacks: []
+        })
         const globalStore = new SubscribableGlobalStore(
             {
+                contentUserStore,
                 treeStore,
                 updatesCallbacks: [],
             }
         )
         const treeStoreOnUpdateSpy = sinon.spy(treeStore, 'onUpdate')
+        const contentUserStoreOnUpdateSpy = sinon.spy(contentUserStore, 'onUpdate')
 
         expect(treeStoreOnUpdateSpy.callCount).to.equal(0)
+        expect(contentUserStoreOnUpdateSpy.callCount).to.equal(0)
         globalStore.startPublishing()
         expect(treeStoreOnUpdateSpy.callCount).to.equal(1)
+        expect(contentUserStoreOnUpdateSpy.callCount).to.equal(1)
     })
 
     it('After calling startPublishing, globalStore should publish updates'
-        + ' when one of its component stores publishes an update', () => {
+        + ' when one of its component stores (treeStore) publishes an update', () => {
         const contentId: ISubscribableMutableField<string> = new SubscribableMutableField<string>()
         const parentId: ISubscribableMutableField<string> = new SubscribableMutableField<string>()
         const children: ISubscribableMutableStringSet = new SubscribableMutableStringSet()
@@ -71,8 +86,14 @@ describe('ISubscribableGlobalStore', () => {
             store: [],
             updatesCallbacks: []
         })
+
+        const contentUserStore: ISubscribableContentUserStore = new SubscribableContentUserStore({
+            store: {},
+            updatesCallbacks: []
+        })
         const globalStore: ISubscribableGlobalStore = new SubscribableGlobalStore(
             {
+                contentUserStore,
                 treeStore,
                 updatesCallbacks: [],
             }
@@ -108,6 +129,72 @@ describe('ISubscribableGlobalStore', () => {
         expect(callback2.getCall(0).args[0].type).to.deep.equal(ObjectDataTypes.TREE_DATA)
 
     })
+
+    it('After calling startPublishing, globalStore should publish updates'
+        + ' when one of its component stores (contentUserStore) publishes an update', () => {
+        const CONTENT_ID = 'efa123'
+
+        const overdue: ISubscribableMutableField<boolean> = new SubscribableMutableField<boolean>()
+        const lastRecordedStrength: ISubscribableMutableField<number> = new SubscribableMutableField<number>()
+        const proficiency: ISubscribableMutableField<PROFICIENCIES> = new SubscribableMutableField<PROFICIENCIES>()
+        const timer: ISubscribableMutableField<number> = new SubscribableMutableField<number>()
+        const contentUser: IMutableSubscribableContentUser = new MutableSubscribableContentUser({
+            lastRecordedStrength,
+            overdue,
+            proficiency,
+            timer,
+            updatesCallbacks: [],
+        })
+        const treeStore: ISubscribableTreeStore = new SubscribableTreeStore( {
+            store: [],
+            updatesCallbacks: []
+        })
+
+        const contentUserStore: ISubscribableContentUserStore = new SubscribableContentUserStore({
+            store: {},
+            updatesCallbacks: []
+        })
+        const globalStore: ISubscribableGlobalStore = new SubscribableGlobalStore(
+            {
+                contentUserStore,
+                treeStore,
+                updatesCallbacks: [],
+            }
+        )
+
+        const callback1 = sinon.spy()
+        const callback2 = sinon.spy()
+        globalStore.onUpdate(callback2)
+        globalStore.onUpdate(callback1)
+
+        globalStore.startPublishing()
+        contentUserStore.startPublishing()
+        contentUser.startPublishing()
+
+        contentUserStore.addAndSubscribeToItem(CONTENT_ID, contentUser)
+
+        const sampleMutation: IIdProppedDatedMutation<ContentUserPropertyMutationTypes, ContentUserPropertyNames> = {
+            data: PROFICIENCIES.THREE,
+            id: CONTENT_ID,
+            propertyName: ContentUserPropertyNames.PROFICIENCY,
+            timestamp: Date.now(),
+            type: FieldMutationTypes.SET,
+        }
+        contentUser.addMutation(sampleMutation)
+
+        const contentUserNewVal = contentUser.val()
+        expect(callback1.callCount).to.equal(1)
+        expect(callback1.getCall(0).args[0].id).to.equal(CONTENT_ID)
+        expect(callback1.getCall(0).args[0].val).to.deep.equal(contentUserNewVal)
+        expect(callback1.getCall(0).args[0].type).to.deep.equal(ObjectDataTypes.CONTENT_USER_DATA)
+        expect(callback2.callCount).to.equal(1)
+        expect(callback2.getCall(0).args[0].id).to.equal(CONTENT_ID)
+        expect(callback2.getCall(0).args[0].val).to.deep.equal(contentUserNewVal)
+        expect(callback2.getCall(0).args[0].type).to.deep.equal(ObjectDataTypes.CONTENT_USER_DATA)
+
+    })
+
+
     //
     it('Before calling startPublishing, globalStore should NOT publish updates ' +
         ' when one of its component stores publishes an update', () => {
@@ -127,8 +214,14 @@ describe('ISubscribableGlobalStore', () => {
         store: {},
         updatesCallbacks: []
     })
+
+    const contentUserStore: ISubscribableContentUserStore = new SubscribableContentUserStore({
+        store: {},
+        updatesCallbacks: []
+    })
     const globalStore: ISubscribableGlobalStore = new SubscribableGlobalStore(
         {
+            contentUserStore,
             treeStore,
             updatesCallbacks: [],
         }
