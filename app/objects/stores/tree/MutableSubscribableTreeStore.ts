0e134f0247bc38c88319e93a4670386c026393f9
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-05 10:29:24 -0800

    Add MutableSubscribableGlobalStore test

--- app/objects/stores/tree/MutableSubscribableTreeStore.ts
+++ app/objects/stores/tree/MutableSubscribableTreeStore.ts
@@ -1,17 +1,43 @@
-import {IIdDatedMutation, IMutableSubscribableTreeStore, TreeMutationTypes} from '../../interfaces';
+import {
+    ContentUserPropertyMutationTypes, ContentUserPropertyNames,
+    IIdDatedMutation, IIdProppedDatedMutation, IMutableSubscribableContentUser, IMutableSubscribableTree,
+    IMutableSubscribableTreeStore,
+    IProppedDatedMutation,
+    TreeMutationTypes,
+    TreePropertyMutationTypes,
+    TreePropertyNames
+} from '../../interfaces';
 import {SubscribableTreeStore} from './SubscribableTreeStore';
 
 class MutableSubscribableTreeStore
     extends SubscribableTreeStore
     implements IMutableSubscribableTreeStore {
-    public addMutation(mutation: IIdDatedMutation<TreeMutationTypes>) {
+    public addMutation(    mutation: IIdProppedDatedMutation<TreePropertyMutationTypes, TreePropertyNames>) {
         // const treeId = mutation.id
         // treeId && this.stores[treeId].addMutation
         // mutation.id
 
+        const id = mutation.id
+        const tree: IMutableSubscribableTree
+            = this.store[id]
+        if (!tree) {
+            throw new RangeError('Couldn\'t find contentUser for contentId' + id)
+        }
+
+        const proppedDatedMutation:
+            IProppedDatedMutation<TreePropertyMutationTypes, TreePropertyNames> = {
+            data: mutation.data,
+            propertyName: mutation.propertyName,
+            timestamp: mutation.timestamp,
+            type: mutation.type,
+        }
+        tree.addMutation(proppedDatedMutation)
+        // throw new Error("Method not implemented.");
     }
 
-    public mutations(): Array<IIdDatedMutation<TreeMutationTypes>> {
+    public mutations(): Array<IIdProppedDatedMutation<TreePropertyMutationTypes, TreePropertyNames>> {
         throw new Error('Not implemented!')
     }
 }
+
+export {MutableSubscribableTreeStore}
