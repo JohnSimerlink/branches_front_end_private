b06c5144a796ca1f41e7ba34707c1569585717ae
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-05 19:13:31 -0800

    Add SubscribableTree tests

--- app/objects/contentUserData/SubscribableContentUser.spec.ts
+++ app/objects/contentUserData/SubscribableContentUser.spec.ts
@@ -1,59 +1,65 @@
-// import {expect} from 'chai'
-// import * as sinon from 'sinon'
-// import {myContainer} from '../../../inversify.config';
-// import {
-//     ISubscribableMutableField,
-//     ISubscribableMutableStringSet,
-// } from '../interfaces';
-// import {TYPES} from '../types';
-// import {SubscribableTree} from './SubscribableTree';
-//
-// describe('SubscribableTree', () => {
-//     it('constructor should set all the subscribable properties', () => {
-//         const contentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const parentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const children = myContainer.get<ISubscribableMutableStringSet>(TYPES.ISubscribableMutableStringSet)
-//         const TREE_ID = 'efa123'
-//         const tree = new SubscribableTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
-//         expect(tree.contentId).to.deep.equal(contentId)
-//         expect(tree.parentId).to.deep.equal(parentId)
-//         expect(tree.children).to.deep.equal(children)
-//         expect(tree.getId()).to.deep.equal(TREE_ID)
-//     })
-//     it('.val() should display the value of the object', () => {
-//         const contentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const parentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const children = myContainer.get<ISubscribableMutableStringSet>(TYPES.ISubscribableMutableStringSet)
-//         const TREE_ID = 'efa123'
-//         const expectedVal = {
-//             children: children.val(),
-//             contentId: contentId.val(),
-//             parentId: parentId.val(),
-//         }
-//         const tree = new SubscribableTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
-//         expect(tree.val()).to.deep.equal(expectedVal)
-//     })
-//     it('.getId() should display the id of the object', () => {
-//         const contentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const parentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const children = myContainer.get<ISubscribableMutableStringSet>(TYPES.ISubscribableMutableStringSet)
-//         const TREE_ID = 'efa123'
-//         const tree = new SubscribableTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
-//         expect(tree.getId()).to.deep.equal(TREE_ID)
-//     })
-//     it('startPublishing() should call the onUpdate methods of all member Subscribable properties', () => {
-//         const contentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const parentId = myContainer.get<ISubscribableMutableField<string>>(TYPES.ISubscribableMutableString)
-//         const children = myContainer.get<ISubscribableMutableStringSet>(TYPES.ISubscribableMutableStringSet)
-//         const TREE_ID = 'efa123'
-//         const tree = new SubscribableTree({updatesCallbacks: [], id: TREE_ID, contentId, parentId, children})
-//         const contentIdOnUpdateSpy = sinon.spy(contentId, 'onUpdate')
-//         const parentIdOnUpdateSpy = sinon.spy(parentId, 'onUpdate')
-//         const childrenOnUpdateSpy = sinon.spy(children, 'onUpdate')
-//         tree.startPublishing()
-//         expect(contentIdOnUpdateSpy.callCount).to.deep.equal(1)
-//         expect(parentIdOnUpdateSpy.callCount).to.deep.equal(1)
-//         expect(childrenOnUpdateSpy.callCount).to.deep.equal(1)
-//     })
-// })
+import {expect} from 'chai'
+import * as sinon from 'sinon'
+import {myContainer} from '../../../inversify.config';
+import {SubscribableMutableField} from '../field/SubscribableMutableField';
+import {
+    ISubscribableMutableField,
+    ISubscribableMutableStringSet,
+} from '../interfaces';
+import {PROFICIENCIES} from '../proficiency/proficiencyEnum';
+import {TYPES} from '../types';
+import {SubscribableContentUser} from './SubscribableContentUser';
 
+describe('SubscribableContentUser', () => {
+    it('constructor should set all the subscribable properties', () => {
+        const overdue = new SubscribableMutableField<boolean>({field: false})
+        const lastRecordedStrength = new SubscribableMutableField<number>({field: 45})
+        const proficiency = new SubscribableMutableField<PROFICIENCIES>({field: PROFICIENCIES.TWO})
+        const timer = new SubscribableMutableField<number>({field: 30})
+        const contentUser = new SubscribableContentUser({
+            lastRecordedStrength, overdue, proficiency, timer, updatesCallbacks: [],
+        })
+        expect(contentUser.overdue).to.deep.equal(overdue)
+        expect(contentUser.timer).to.deep.equal(timer)
+        expect(contentUser.proficiency).to.deep.equal(proficiency)
+        expect(contentUser.lastRecordedStrength).to.deep.equal(lastRecordedStrength)
+    })
+    it('.val() should display the value of the object', () => {
+        const lastRecordedStrength = new SubscribableMutableField<number>({field: 45})
+        const overdue = new SubscribableMutableField<boolean>({field: false})
+        const proficiency = new SubscribableMutableField<PROFICIENCIES>({field: PROFICIENCIES.TWO})
+        const timer = new SubscribableMutableField<number>({field: 30})
+        const contentUser = new SubscribableContentUser({
+            lastRecordedStrength, overdue, proficiency, timer, updatesCallbacks: [],
+        })
+
+        const expectedVal = {
+            lastRecordedStrength: lastRecordedStrength.val(),
+            overdue: overdue.val(),
+            proficiency: proficiency.val(),
+            timer: timer.val(),
+        }
+
+        expect(contentUser.val()).to.deep.equal(expectedVal)
+    })
+    it('startPublishing() should call the onUpdate methods of all member Subscribable properties', () => {
+        const lastRecordedStrength = new SubscribableMutableField<number>({field: 45})
+        const overdue = new SubscribableMutableField<boolean>({field: false})
+        const proficiency = new SubscribableMutableField<PROFICIENCIES>({field: PROFICIENCIES.TWO})
+        const timer = new SubscribableMutableField<number>({field: 30})
+        const contentUser = new SubscribableContentUser({
+            lastRecordedStrength, overdue, proficiency, timer, updatesCallbacks: [],
+        })
+
+        const lastRecordedStrengthOnUpdateSpy = sinon.spy(lastRecordedStrength, 'onUpdate')
+        const overdueOnUpdateSpy = sinon.spy(overdue, 'onUpdate')
+        const proficiencyOnUpdateSpy = sinon.spy(proficiency, 'onUpdate')
+        const timerOnUpdateSpy = sinon.spy(timer, 'onUpdate')
+
+        contentUser.startPublishing()
+        expect(overdueOnUpdateSpy.callCount).to.deep.equal(1)
+        expect(lastRecordedStrengthOnUpdateSpy.callCount).to.deep.equal(1)
+        expect(timerOnUpdateSpy.callCount).to.deep.equal(1)
+        expect(proficiencyOnUpdateSpy.callCount).to.deep.equal(1)
+    })
+})
