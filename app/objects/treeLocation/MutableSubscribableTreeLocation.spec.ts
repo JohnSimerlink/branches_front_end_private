d127177c6152f2f8c9f5b7b75fe47f48a556802c
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-06 16:28:24 -0800

    Pass rest of MutableSubscribableTreeLocation tests

--- app/objects/treeLocation/MutableSubscribableTreeLocation.spec.ts
+++ app/objects/treeLocation/MutableSubscribableTreeLocation.spec.ts
@@ -86,80 +86,57 @@ describe('MutableSubscribableTreeLocation', () => {
         expect(calledWith).to.deep.equal(newTreeLocationDataValue)
     })
 
-    // it('a mutation in one of the subscribable properties' +
-    //     ' should NOT publish an update of the entire object\'s value'
-    //     + ' before startPublishing has been called', () => {
-    //
-    //     const proficiencyStatsVal: IProficiencyStats = {
-    //         UNKNOWN: 3,
-    //         ONE: 2,
-    //         TWO: 3,
-    //         THREE: 4,
-    //         FOUR: 2,
-    //     }
-    //     const newProficiencyStatsVal: IProficiencyStats = {
-    //         UNKNOWN: 3,
-    //         ONE: 6,
-    //         TWO: 3,
-    //         THREE: 4,
-    //         FOUR: 2,
-    //     }
-    //     const aggregationTimerVal = 54
-    //     const proficiencyStats = new SubscribableMutableField<IProficiencyStats>({field: proficiencyStatsVal})
-    //     const aggregationTimer = new SubscribableMutableField<number>({field: aggregationTimerVal})
-    //     const treeLocation = new MutableSubscribableTreeLocation({updatesCallbacks: [], proficiencyStats, aggregationTimer})
-    //
-    //     const callback = sinon.spy()
-    //     treeLocation.onUpdate(callback)
-    //
-    //     const sampleMutation: IDatedMutation<FieldMutationTypes> = {
-    //         data: PROFICIENCIES.THREE,
-    //         timestamp: Date.now(),
-    //         type: FieldMutationTypes.SET,
-    //     }
-    //     proficiencyStats.addMutation(sampleMutation)
-    //     expect(callback.callCount).to.equal(0)
-    // })
-    // it('addMutation ' +
-    //     ' should call addMutation on the appropriate descendant property' +
-    //     'and that mutation called on the descendant property should no longer have the propertyName on it', () => {
-    //     const proficiencyStatsVal: IProficiencyStats = {
-    //         UNKNOWN: 3,
-    //         ONE: 2,
-    //         TWO: 3,
-    //         THREE: 4,
-    //         FOUR: 2,
-    //     }
-    //     const newProficiencyStatsVal: IProficiencyStats = {
-    //         UNKNOWN: 3,
-    //         ONE: 6,
-    //         TWO: 3,
-    //         THREE: 4,
-    //         FOUR: 2,
-    //     }
-    //     const aggregationTimerVal = 54
-    //     const proficiencyStats = new SubscribableMutableField<IProficiencyStats>({field: proficiencyStatsVal})
-    //     const aggregationTimer = new SubscribableMutableField<number>({field: aggregationTimerVal})
-    //     const treeLocation = new MutableSubscribableTreeLocation({updatesCallbacks: [], proficiencyStats, aggregationTimer})
-    //
-    //     const callback = sinon.spy()
-    //     treeLocation.onUpdate(callback)
-    //
-    //     const mutationWithoutPropName: IDatedMutation<FieldMutationTypes> = {
-    //         data: PROFICIENCIES.FOUR,
-    //         timestamp: Date.now(),
-    //         type: FieldMutationTypes.SET
-    //     }
-    //     const mutation: IProppedDatedMutation<FieldMutationTypes, TreeLocationPropertyNames> = {
-    //         ...mutationWithoutPropName,
-    //         propertyName: TreeLocationPropertyNames.PROFICIENCY_STATS,
-    //     }
-    //     const proficiencyStatsAddMutationSpy = sinon.spy(proficiencyStats, 'addMutation')
-    //     treeLocation.addMutation(mutation)
-    //     expect(proficiencyStatsAddMutationSpy.callCount).to.equal(1)
-    //     const calledWith = proficiencyStatsAddMutationSpy.getCall(0).args[0]
-    //     expect(calledWith).to.deep.equal(mutationWithoutPropName)
-    //
-    // })
-    //
+    it('a mutation in one of the subscribable properties' +
+        ' should NOT publish an update of the entire object\'s value'
+        + ' before startPublishing has been called', () => {
+
+        const FIRST_POINT_VALUE = {x: 5, y: 7}
+        const MUTATION_VALUE = {x: 3, y: 4}
+        const SECOND_POINT_VALUE = {
+            x: FIRST_POINT_VALUE.x + MUTATION_VALUE.x,
+            y: FIRST_POINT_VALUE.y + MUTATION_VALUE.y
+        }
+        const point: ISubscribableUndoableMutablePoint
+            = new SubscribableMutablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
+
+        const treeLocation = new MutableSubscribableTreeLocation({updatesCallbacks: [], point})
+        const callback = sinon.spy()
+        treeLocation.onUpdate(callback)
+
+        const sampleMutation: IDatedMutation<PointMutationTypes> = {
+            data: {delta: MUTATION_VALUE},
+            timestamp: Date.now(),
+            type: PointMutationTypes.SHIFT,
+        }
+        point.addMutation(sampleMutation)
+        expect(callback.callCount).to.equal(0)
+    })
+    it('addMutation ' +
+        ' should call addMutation on the appropriate descendant property' +
+        'and that mutation called on the descendant property should no longer have the propertyName on it', () => {
+        const FIRST_POINT_VALUE = {x: 5, y: 7}
+        const MUTATION_VALUE = {delta: {x: 3, y: 4}}
+        const point: ISubscribableUndoableMutablePoint
+            = new SubscribableMutablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
+
+        const treeLocation = new MutableSubscribableTreeLocation({updatesCallbacks: [], point})
+
+        const callback = sinon.spy()
+        treeLocation.onUpdate(callback)
+
+        const mutationWithoutPropName: IDatedMutation<PointMutationTypes> = {
+            data: MUTATION_VALUE,
+            timestamp: Date.now(),
+            type: PointMutationTypes.SHIFT
+        }
+        const mutation: IProppedDatedMutation<PointMutationTypes, TreeLocationPropertyNames> = {
+            ...mutationWithoutPropName,
+            propertyName: TreeLocationPropertyNames.POINT,
+        }
+        const pointAddMutationSpy = sinon.spy(point, 'addMutation')
+        treeLocation.addMutation(mutation)
+        expect(pointAddMutationSpy.callCount).to.equal(1)
+        const calledWith = pointAddMutationSpy.getCall(0).args[0]
+        expect(calledWith).to.deep.equal(mutationWithoutPropName)
+    })
 })
