6dc48fc55790fe66bb9481545678dbe1cdb78b85
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-07 20:13:54 -0800

    Enable importation of Vue/Vuex in a typescript file

---
+++ app/core/store2.ts
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+    uri: null,
+};
+
+const getters = {
+}
+const mutations = {
+}
+const actions = {}
+
+const store = new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters,
+})
