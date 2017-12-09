1bf66d586074e50faebaa15b76887d97d63b1d9a
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-08 20:33:48 -0800

    Switch to other dependency injection checker

--- app/objects/sigmaNode/sigmaRenderManager.spec.ts
+++ app/objects/sigmaNode/sigmaRenderManager.spec.ts
@@ -1,15 +1,19 @@
 import {expect} from 'chai'
-import {ISigmaRenderManager} from '../interfaces';
-import {SigmaRenderManager} from './SigmaRenderManager';
 import {myContainer} from '../../../inversify.config';
+import {injectionWorks} from '../../testHelpers/testHelpers';
+import {ISigmaRenderManager} from '../interfaces';
 import {TYPES} from '../types';
+import {SigmaRenderManager, SigmaRenderManagerArgs} from './SigmaRenderManager';
 
 describe('SigmaRenderManager', () => {
     it('DI constructor works', () => {
 
-        const sigmaRenderManager: ISigmaRenderManager = myContainer.get<ISigmaRenderManager>(TYPES.ISigmaRenderManager)
-        expect(sigmaRenderManager['treeDataLoadedIdsSet']).to.not.equal(undefined)
-        expect(sigmaRenderManager['treeLocationDataLoadedIdsSet']).to.not.equal(undefined)
+        const injects = injectionWorks<SigmaRenderManagerArgs, ISigmaRenderManager>({
+            container: myContainer,
+            argsType: TYPES.SigmaRenderManagerArgs,
+            classType: TYPES.ISigmaRenderManager,
+        })
+        expect(injects).to.equal(true)
     })
     it('should mark as renderable after marking tree and treelocation data added[Regular Constructor]', () => {
         const treeDataLoadedIdsSet = {}
