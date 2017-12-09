acc0578f153a429a2aea4987cf5eaf004d1aa56b
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-09 09:59:40 -0800

    Add /buy Route to add stripe test integration

--- app/core/bootstrap.js
+++ app/core/bootstrap.js
@@ -26,12 +26,14 @@ import store from './store.js'
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 
+const Buy = { template: '<div> Give us ur monee</div>'}
 // 2. Define some routes
 // Each route should map to a component. The "component" can
 // either be an actual component constructor created via
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
 const routes = [
+  { path: '/buy', component: Buy },
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar },
   { path: '/study/:leafId', name: 'study', component: TreeReviewContainer, props: true },
