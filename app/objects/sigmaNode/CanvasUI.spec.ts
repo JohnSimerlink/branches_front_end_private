f1abfba780073934ac131dc0a96509afeaddb8fd
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-12 16:35:58 -0800

    Remove unused code

--- app/objects/sigmaNode/CanvasUI.spec.ts
+++ app/objects/sigmaNode/CanvasUI.spec.ts
@@ -2,27 +2,18 @@ import {expect} from 'chai'
 import * as sinon from 'sinon'
 import {myContainer} from '../../../inversify.config';
 import {
-    ISubscribable, ISubscribableContentStore, ISubscribableContentUserStore, ISubscribableGlobalStore,
-    ISubscribableTreeLocationStore,
-    ISubscribableTreeStore,
-    ISubscribableTreeUserStore,
+    ISubscribable, ISubscribableGlobalStore,
     ITypeAndIdAndValUpdates
 } from '../interfaces';
-import {SubscribableContentUserStore} from '../stores/contentUser/SubscribableContentUserStore';
-import {SubscribableGlobalStore} from '../stores/SubscribableGlobalStore';
-import {SubscribableTreeStore} from '../stores/tree/SubscribableTreeStore';
 import {TYPES} from '../types';
 import {CanvasUI} from './CanvasUI';
-import {SubscribableTreeUserStore} from '../stores/treeUser/SubscribableTreeUserStore';
-import {SubscribableContentStore} from '../stores/content/SubscribableContentStore';
-import {SubscribableTreeLocationStore} from '../stores/treeLocation/SubscribableTreeLocationStore';
 
 describe('CanvasUI', () => {
     it('should subscribe to the onUpdate method of a subscribable obj passed to it', () => {
         const canvasUI = myContainer.get<CanvasUI>(TYPES.CanvasUI)
 
-        const subscribable: ISubscribable<ITypeAndIdAndValUpdates> = myContainer.get<ISubscribableGlobalStore>(TYPES.ISubscribableGlobalStore)
-        // const subscribable = myContainer.get<SubscribableGlobalStore>(TYPES.ISubscribableGlobalStore)
+        const subscribable: ISubscribable<ITypeAndIdAndValUpdates>
+            = myContainer.get<ISubscribableGlobalStore>(TYPES.ISubscribableGlobalStore)
 
         const subscribableOnUpdateSpy = sinon.spy(subscribable, 'onUpdate')
 
