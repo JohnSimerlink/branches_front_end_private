3c8644097932f5cd7258dc46e3d5616d07f64c94
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-16 18:47:53 -0500

    Convert app/core folder to ava tests

--- app/core/app.integration2.spec.ts
+++ app/core/app.integration2.spec.ts
@@ -1,3 +1,4 @@
+import test from 'ava'
 import {expect} from 'chai'
 import {MockFirebase} from 'firebase-mock'
 import * as sinon from 'sinon'
@@ -5,15 +6,15 @@ import {myContainer} from '../../inversify.config';
 import {FIREBASE_PATHS} from '../loaders/paths';
 import {TreeLoader} from '../loaders/tree/TreeLoader';
 import {TreeLocationLoader} from '../loaders/treeLocation/TreeLocationLoader';
-import {
-    IHash,
-    IRenderedNodesManagerCore,
-    ISigmaNode, ISigmaNodesUpdater, IStoreSourceUpdateListenerCore,
-} from '../objects/interfaces';
 import {ITreeDataWithoutId, ITreeLocationData} from '../objects/interfaces';
 import {
     IRenderedNodesManager,
     IStoreSourceUpdateListener
+} from '../objects/interfaces';
+import {
+    IHash,
+    IRenderedNodesManagerCore,
+    ISigmaNode, ISigmaNodesUpdater, IStoreSourceUpdateListenerCore,
 } from '../objects/interfaces';
 import {ISigmaRenderManager, ISubscribableTreeLocationStoreSource, ISubscribableTreeStoreSource} from '../objects/interfaces';
 import {RenderedNodesManager} from '../objects/sigmaNode/RenderedNodesManager';
@@ -24,76 +25,76 @@ import {StoreSourceUpdateListenerCore} from '../objects/stores/StoreSourceUpdate
 import {TYPES} from '../objects/types';
 import {TREE_ID} from '../testHelpers/testHelpers';
 
-describe('App integration test 2 - loadTree/loadTreeLocation -> renderedSigmaNodes', () => {
-    it('once a tree/treeLocation is loaded,' +
-        ' that treeId should appear as a node in the renderedSigmaNodes set', async () => {
-        const treeIdToDownload = TREE_ID
+test('App integration test 2 - loadTree/loadTreeLocation -> renderedSigmaNodes::::: ' +
+    'once a tree/treeLocation is loaded,' +
+    ' that treeId should appear as a node in the renderedSigmaNodes set', async (t) => {
+    const treeIdToDownload = TREE_ID
 
-        const sampleTreeData: ITreeDataWithoutId = {
-            contentId: '12345532',
-            parentId: '493284',
-            children: ['2948, 2947']
+    const sampleTreeData: ITreeDataWithoutId = {
+        contentId: '12345532',
+        parentId: '493284',
+        children: ['2948, 2947']
+    }
+
+    const sampleTreeLocationData: ITreeLocationData = {
+        point: {
+            x: 5,
+            y: 8,
         }
+    }
 
-        const sampleTreeLocationData: ITreeLocationData = {
-            point: {
-                x: 5,
-                y: 8,
-            }
-        }
+    const firebaseTreesRef = new MockFirebase(FIREBASE_PATHS.TREES)
+    const treeRef = firebaseTreesRef.child(treeIdToDownload)
+    const firebaseTreeLocationsRef = new MockFirebase(FIREBASE_PATHS.TREE_LOCATIONS)
+    const treeLocationRef = firebaseTreeLocationsRef.child(treeIdToDownload)
 
-        const firebaseTreesRef = new MockFirebase(FIREBASE_PATHS.TREES)
-        const treeRef = firebaseTreesRef.child(treeIdToDownload)
-        const firebaseTreeLocationsRef = new MockFirebase(FIREBASE_PATHS.TREE_LOCATIONS)
-        const treeLocationRef = firebaseTreeLocationsRef.child(treeIdToDownload)
+    const treeStoreSource: ISubscribableTreeStoreSource
+        = myContainer.get<ISubscribableTreeStoreSource>(TYPES.ISubscribableTreeStoreSource)
+    const treeLocationStoreSource: ISubscribableTreeLocationStoreSource
+        = myContainer.get<ISubscribableTreeLocationStoreSource>
+    (TYPES.ISubscribableTreeLocationStoreSource)
+    const treeLoader = new TreeLoader({firebaseRef: firebaseTreesRef, storeSource: treeStoreSource})
+    const treeLocationLoader
+        = new TreeLocationLoader({firebaseRef: firebaseTreeLocationsRef, storeSource: treeLocationStoreSource})
 
-        const treeStoreSource: ISubscribableTreeStoreSource
-            = myContainer.get<ISubscribableTreeStoreSource>(TYPES.ISubscribableTreeStoreSource)
-        const treeLocationStoreSource: ISubscribableTreeLocationStoreSource
-            = myContainer.get<ISubscribableTreeLocationStoreSource>
-        (TYPES.ISubscribableTreeLocationStoreSource)
-        const treeLoader = new TreeLoader({firebaseRef: firebaseTreesRef, storeSource: treeStoreSource})
-        const treeLocationLoader
-            = new TreeLocationLoader({firebaseRef: firebaseTreeLocationsRef, storeSource: treeLocationStoreSource})
+    const sigmaNodes: IHash<ISigmaNode> = {}
 
-        const sigmaNodes: IHash<ISigmaNode> = {}
+    const sigmaRenderManager: ISigmaRenderManager = myContainer.get<ISigmaRenderManager>(TYPES.ISigmaRenderManager)
+    const sigmaNodesUpdater: ISigmaNodesUpdater
+        = new SigmaNodesUpdater({sigmaNodes, sigmaRenderManager, getSigmaIdsForContentId: () => void 0})
 
-        const sigmaRenderManager: ISigmaRenderManager = myContainer.get<ISigmaRenderManager>(TYPES.ISigmaRenderManager)
-        const sigmaNodesUpdater: ISigmaNodesUpdater
-            = new SigmaNodesUpdater({sigmaNodes, sigmaRenderManager, getSigmaIdsForContentId: () => void 0})
+    const storeSourceUpdateListenerCore: IStoreSourceUpdateListenerCore
+        = new StoreSourceUpdateListenerCore({sigmaNodes, sigmaNodesUpdater})
+    const storeSourceUpdateListener: IStoreSourceUpdateListener
+        = new StoreSourceUpdateListener({storeSourceUpdateListenerCore})
+    const renderedNodes: IHash<ISigmaNode> = {}
+    const renderedNodesManagerCore: IRenderedNodesManagerCore
+        = new RenderedNodesManagerCore({allSigmaNodes: sigmaNodes, renderedNodes})
+    const renderedNodesManager: IRenderedNodesManager = new RenderedNodesManager({renderedNodesManagerCore})
+    renderedNodesManager.subscribe(sigmaRenderManager)
 
-        const storeSourceUpdateListenerCore: IStoreSourceUpdateListenerCore
-            = new StoreSourceUpdateListenerCore({sigmaNodes, sigmaNodesUpdater})
-        const storeSourceUpdateListener: IStoreSourceUpdateListener
-            = new StoreSourceUpdateListener({storeSourceUpdateListenerCore})
-        const renderedNodes: IHash<ISigmaNode> = {}
-        const renderedNodesManagerCore: IRenderedNodesManagerCore
-            = new RenderedNodesManagerCore({allSigmaNodes: sigmaNodes, renderedNodes})
-        const renderedNodesManager: IRenderedNodesManager = new RenderedNodesManager({renderedNodesManagerCore})
-        renderedNodesManager.subscribe(sigmaRenderManager)
+    storeSourceUpdateListener.subscribe(treeStoreSource)
+    storeSourceUpdateListener.subscribe(treeLocationStoreSource)
 
-        storeSourceUpdateListener.subscribe(treeStoreSource)
-        storeSourceUpdateListener.subscribe(treeLocationStoreSource)
+    const treeStoreSourceCallCallbacks = sinon.spy(treeStoreSource, 'callCallbacks')
 
-        const treeStoreSourceCallCallbacks = sinon.spy(treeStoreSource, 'callCallbacks')
+    let inRenderedSet: boolean = !!renderedNodes[treeIdToDownload]
+    expect(inRenderedSet).to.equal(false)
+    treeRef.fakeEvent('value', undefined, sampleTreeData)
+    treeLoader.downloadData(treeIdToDownload)
+    treeRef.flush()
+    inRenderedSet = !!renderedNodes[treeIdToDownload]
+    expect(treeStoreSourceCallCallbacks.callCount).to.equal(1)
+    // expect(sigmaNodeCreatorReceiveUpdateSpy.callCount).to.equal(1)
+    expect(inRenderedSet).to.equal(false)
 
-        let inRenderedSet: boolean = !!renderedNodes[treeIdToDownload]
-        expect(inRenderedSet).to.equal(false)
-        treeRef.fakeEvent('value', undefined, sampleTreeData)
-        treeLoader.downloadData(treeIdToDownload)
-        treeRef.flush()
-        inRenderedSet = !!renderedNodes[treeIdToDownload]
-        expect(treeStoreSourceCallCallbacks.callCount).to.equal(1)
-        // expect(sigmaNodeCreatorReceiveUpdateSpy.callCount).to.equal(1)
-        expect(inRenderedSet).to.equal(false)
+    treeLocationRef.fakeEvent('value', undefined, sampleTreeLocationData)
+    treeLocationLoader.downloadData(treeIdToDownload)
+    treeLocationRef.flush()
+    // expect(sigmaNodeCreatorReceiveUpdateSpy.callCount).to.equal(2)
 
-        treeLocationRef.fakeEvent('value', undefined, sampleTreeLocationData)
-        treeLocationLoader.downloadData(treeIdToDownload)
-        treeLocationRef.flush()
-        // expect(sigmaNodeCreatorReceiveUpdateSpy.callCount).to.equal(2)
+    inRenderedSet = !!renderedNodes[treeIdToDownload]
+    expect(inRenderedSet).to.equal(true)
+    t.pass()
 
-        inRenderedSet = !!renderedNodes[treeIdToDownload]
-        expect(inRenderedSet).to.equal(true)
-
-    })
 })
--- app/core/newUtils.spec.ts
+++ app/core/newUtils.spec.ts
@@ -1,38 +1,41 @@
+import test from 'ava'
 import {expect} from 'chai'
 import {stringArrayToSet, stripTrailingSlash} from './newUtils';
 
-describe('stripTrailingSlash', () => {
-    it('should return falsey for blank inputs', () => {
-        let falsey = !stripTrailingSlash('')
-        expect(falsey).to.equal(true)
-        falsey = !stripTrailingSlash(null)
-        expect(falsey).to.equal(true)
-    })
-    it('should return blank for /', () => {
-        const result = stripTrailingSlash('/')
-        expect(result).to.equal('')
-    })
-    it('should strip slashes for other values', () => {
-        let result = stripTrailingSlash('people/tai/')
-        expect(result).to.equal('people/tai')
-        result = stripTrailingSlash('books/goodbooks/thethirdwave/')
-        expect(result).to.equal('books/goodbooks/thethirdwave')
-    })
-    it('should keep the same when no trailing slash', () => {
-        let result = stripTrailingSlash('people/tai')
-        expect(result).to.equal('people/tai')
-        result = stripTrailingSlash('books/goodbooks/thethirdwave')
-        expect(result).to.equal('books/goodbooks/thethirdwave')
-    })
+test('stripTrailingSlash::::should return falsey for blank inputs', (t) => {
+    let falsey = !stripTrailingSlash('')
+    expect(falsey).to.equal(true)
+    falsey = !stripTrailingSlash(null)
+    expect(falsey).to.equal(true)
+    t.pass()
+})
+test('stripTrailingSlash::::should return blank for /', (t) => {
+    const result = stripTrailingSlash('/')
+    expect(result).to.equal('')
+    t.pass()
+})
+test('stripTrailingSlash::::should strip slashes for other values', (t) => {
+    let result = stripTrailingSlash('people/tai/')
+    expect(result).to.equal('people/tai')
+    result = stripTrailingSlash('books/goodbooks/thethirdwave/')
+    expect(result).to.equal('books/goodbooks/thethirdwave')
+    t.pass()
+})
+test('stripTrailingSlash::::should keep the same when no trailing slash', (t) => {
+    let result = stripTrailingSlash('people/tai')
+    expect(result).to.equal('people/tai')
+    result = stripTrailingSlash('books/goodbooks/thethirdwave')
+    expect(result).to.equal('books/goodbooks/thethirdwave')
+    t.pass()
 })
 
-describe('stringArrayToSet', () => {
-    it('should return {} for []', () => {
-        const set = stringArrayToSet([])
-        expect(set).to.deep.equal({})
-    })
-    it('should return {a: true, b: true} for ["a",b"]', () => {
-        const set = stringArrayToSet(['a', 'b'])
-        expect(set).to.deep.equal({a: true, b: true})
-    })
+test('stringArrayToSet::::should return {} for []', (t) => {
+    const set = stringArrayToSet([])
+    expect(set).to.deep.equal({})
+    t.pass()
+})
+test('stringArrayToSet::::should return {a: true, b: true} for ["a",b"]', (t) => {
+    const set = stringArrayToSet(['a', 'b'])
+    expect(set).to.deep.equal({a: true, b: true})
+    t.pass()
 })
