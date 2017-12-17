adb5b2f264ec91fa977d99da580e78ac1e1a7b6f
JohnSimerlink <johnsimerlink@gmail.com>
2017-12-17 13:18:15 -0500

    Refactor point, proficiencyStats, and set tests to Ava

--- app/objects/point/MutableSubscribablePoint.spec.ts
+++ app/objects/point/MutableSubscribablePoint.spec.ts
@@ -1,3 +1,4 @@
+import test from 'ava'
 import {expect} from 'chai'
 import 'reflect-metadata'
 import * as sinon from 'sinon'
@@ -23,147 +24,152 @@ TODO: For MY Mutation tests, for performance reasons,
  - Being able to get the current coordinate value
  - Having those 3 specs appropriately adjust the coordinate value
   */
-describe('Point > Mutable', () => {
-    // FIRST_MUTATION_VALUE is {x: 5, y: 7}
-    // const po = new MutableSubscribablePoint({x:5, y:6})
-    const FIRST_POINT_VALUE = {x: 5, y: 7}
-    const FIRST_MUTATION_VALUE = {x: 3, y: 4}
-    const SECOND_POINT_VALUE = {
-        x: FIRST_POINT_VALUE.x + FIRST_MUTATION_VALUE.x, y: FIRST_POINT_VALUE.y + FIRST_MUTATION_VALUE.y
-    }
-    const SECOND_MUTATION_VALUE = {x: -3, y: -4}
-    const THIRD_POINT_VALUE = {
-        x: SECOND_POINT_VALUE.x + SECOND_MUTATION_VALUE.x, y: SECOND_POINT_VALUE.y + SECOND_MUTATION_VALUE.y
-    }
-    const THIRD_MUTATION_VALUE = {x: 13, y: 14}
-    const FOURTH_POINT_VALUE = {
-        x: THIRD_POINT_VALUE.x + THIRD_MUTATION_VALUE.x, y: THIRD_POINT_VALUE.y + THIRD_MUTATION_VALUE.y
-    }
-    const FOURTH_MUTATION_VALUE = {x: 100, y: 100}
-    const FIFTH_POINT_VALUE = {
-        x: FOURTH_POINT_VALUE.x + FOURTH_MUTATION_VALUE.x, y: FOURTH_POINT_VALUE.y + FOURTH_MUTATION_VALUE.y
-    }
-    const FIFTH_MINUS_1ST_AND_3RD_MUTATIONS_VALUE = {
-        x: FIFTH_POINT_VALUE.x - THIRD_MUTATION_VALUE.x - FIRST_MUTATION_VALUE.x,
-        y: FIFTH_POINT_VALUE.y - THIRD_MUTATION_VALUE.y - FIRST_MUTATION_VALUE.y
-    }
-    const FIFTH_MINUS_1ST_MUTATION_VALUE = {
-        x: FIFTH_POINT_VALUE.x - FIRST_MUTATION_VALUE.x,
-        y: FIFTH_POINT_VALUE.y - FIRST_MUTATION_VALUE.y
-    }
-    // const point = new MutableSubscribablePoint(FIRST_POINT_VALUE.x, FIRST_POINT_VALUE.y)
-    const point = new MutableSubscribablePoint({...FIRST_POINT_VALUE, updatesCallbacks: []})
-    const FIRST_MUTATION_INDEX = 0
-    const SECOND_MUTATION_INDEX = 1
-    const THIRD_MUTATION_INDEX = 2
-    const FOURTH_MUTATION_INDEX = 3
-    it('should set x and y from constructor', () => {
-        expect(point.val()).to.deep.equal(FIRST_POINT_VALUE)
-    })
-
-    it('should have 0 mutations after creation', () => {
-        expect(point.mutations().length).to.equal(0)
-        // TODO: ^^ Fix Violation of Law of Demeter
-    } )
-
-    // const firstMutation = point.mutations(FIRST_MUTATION_INDEX)
-    it(`second mutation should change the point val
-    to the second point val,
-    and mutation result should have the result val`, () => {
-        const mutation = {type: PointMutationTypes.SHIFT, timestamp: Date.now(), data: {delta: FIRST_MUTATION_VALUE}}
-        point.addMutation(mutation)
-        expect(point.mutations().length).to.equal(1)
-        // TODO: ^^ Fix Violation of Law of Demeter
-        expect(point.val()).to.deep.equal(SECOND_POINT_VALUE)
-    })
-    it(`should error on trying to be redo a mutation that hasn't
-     been undone PT2, and point val should still be the same`, () => {
-        expect(() => point.redo(FIRST_MUTATION_INDEX)).to.throw(RangeError)
-    } )
-    it(`undoing the second mutation should make point equivalent
-     to the original value second mutation`, () => {
-        point.undo(FIRST_MUTATION_INDEX)
-        expect(point.val()).to.deep.equal(FIRST_POINT_VALUE)
-    })
-    it(`redoing the second mutation should make point equivalent
-     to the second point value`, () => {
-        point.redo(FIRST_MUTATION_INDEX)
-        expect(point.val()).to.deep.equal(SECOND_POINT_VALUE)
-    })
-    it(`adding the third and fourth and fifth mutation should make the point
-    equivalent to the fifth point value`, () => {
-        const thirdMutation = {
-            data: {delta: SECOND_MUTATION_VALUE},
-            timestamp: Date.now(),
-            type: PointMutationTypes.SHIFT,
-        }
-        const fourthMutation = {
-            data: {delta: THIRD_MUTATION_VALUE},
-            timestamp: Date.now(),
-            type: PointMutationTypes.SHIFT,
-        }
-        const fifthMutation = {
-            data: {delta: FOURTH_MUTATION_VALUE},
-            timestamp: Date.now(),
-            type: PointMutationTypes.SHIFT,
-        }
-        point.addMutation(thirdMutation)
-        point.addMutation(fourthMutation)
-        point.addMutation(fifthMutation)
-        expect(point.val()).to.deep.equal(FIFTH_POINT_VALUE)
-    })
-    it(`undoing the second and fourth mutations should recalculate
-     the state as the sum of the 1st, third, and 5th mutations`, () => {
-        point.undo(FIRST_MUTATION_INDEX)
-        point.undo(THIRD_MUTATION_INDEX)
-        expect(point.val()).to.deep.equal(FIFTH_MINUS_1ST_AND_3RD_MUTATIONS_VALUE)
-    })
-    it(`redoing the second and fourth mutations should recalculate
-     the state as the sum of the 1st, third, fourth, and 5th mutations`, () => {
-        point.redo(THIRD_MUTATION_INDEX)
-        expect(point.val()).to.deep.equal(FIFTH_MINUS_1ST_MUTATION_VALUE)
-    })
-    it(`should error on trying to be redo a mutation that hasn't
-     been undone (or that has been redone),
-     and point val should still be the same`, () => {
-        expect(() => point.redo(THIRD_MUTATION_INDEX)).to.throw(RangeError)
-    })
+// FIRST_MUTATION_VALUE is {x: 5, y: 7}
+// const po = new MutableSubscribablePoint({x:5, y:6})
+const FIRST_POINT_VALUE = {x: 5, y: 7}
+const FIRST_MUTATION_VALUE = {x: 3, y: 4}
+const SECOND_POINT_VALUE = {
+    x: FIRST_POINT_VALUE.x + FIRST_MUTATION_VALUE.x, y: FIRST_POINT_VALUE.y + FIRST_MUTATION_VALUE.y
+}
+const SECOND_MUTATION_VALUE = {x: -3, y: -4}
+const THIRD_POINT_VALUE = {
+    x: SECOND_POINT_VALUE.x + SECOND_MUTATION_VALUE.x, y: SECOND_POINT_VALUE.y + SECOND_MUTATION_VALUE.y
+}
+const THIRD_MUTATION_VALUE = {x: 13, y: 14}
+const FOURTH_POINT_VALUE = {
+    x: THIRD_POINT_VALUE.x + THIRD_MUTATION_VALUE.x, y: THIRD_POINT_VALUE.y + THIRD_MUTATION_VALUE.y
+}
+const FOURTH_MUTATION_VALUE = {x: 100, y: 100}
+const FIFTH_POINT_VALUE = {
+    x: FOURTH_POINT_VALUE.x + FOURTH_MUTATION_VALUE.x, y: FOURTH_POINT_VALUE.y + FOURTH_MUTATION_VALUE.y
+}
+const FIFTH_MINUS_1ST_AND_3RD_MUTATIONS_VALUE = {
+    x: FIFTH_POINT_VALUE.x - THIRD_MUTATION_VALUE.x - FIRST_MUTATION_VALUE.x,
+    y: FIFTH_POINT_VALUE.y - THIRD_MUTATION_VALUE.y - FIRST_MUTATION_VALUE.y
+}
+const FIFTH_MINUS_1ST_MUTATION_VALUE = {
+    x: FIFTH_POINT_VALUE.x - FIRST_MUTATION_VALUE.x,
+    y: FIFTH_POINT_VALUE.y - FIRST_MUTATION_VALUE.y
+}
+// const point = new MutableSubscribablePoint(FIRST_POINT_VALUE.x, FIRST_POINT_VALUE.y)
+const point = new MutableSubscribablePoint({...FIRST_POINT_VALUE, updatesCallbacks: []})
+const FIRST_MUTATION_INDEX = 0
+const SECOND_MUTATION_INDEX = 1
+const THIRD_MUTATION_INDEX = 2
+const FOURTH_MUTATION_INDEX = 3
+test('Point > Mutable:::should set x and y from constructor', (t) => {
+    expect(point.val()).to.deep.equal(FIRST_POINT_VALUE)
+    t.pass()
 })
 
-describe('Point > Subscribable', () => {
-    it('Adding a mutation, should trigger an update for one of the subscribers ', () => {
+test('Point > Mutable:::should have 0 mutations after creation', (t) => {
+    expect(point.mutations().length).to.equal(0)
+    // TODO: ^^ Fix Violation of Law of Demeter
+    t.pass()
+})
 
-        const FIRST_POINT_VALUE = {x: 5, y: 7}
-        const MUTATION_VALUE = {x: 3, y: 4}
-        const subscribableMutablePoint: IMutableSubscribablePoint
-            = new MutableSubscribablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
-        const callback = sinon.spy() // (updates: IDetailedUpdates) => void 0
-        const mutation: IDatedMutation<PointMutationTypes> = {
-            timestamp: Date.now(),
-            type: PointMutationTypes.SHIFT,
-            data: {delta: {...MUTATION_VALUE}},
-        }
+// const firstMutation = point.mutations(FIRST_MUTATION_INDEX)
+test(`Point > Mutable:::second mutation should change the point val
+    to the second point val,
+    and mutation result should have the result val`, (t) => {
+    const mutation = {type: PointMutationTypes.SHIFT, timestamp: Date.now(), data: {delta: FIRST_MUTATION_VALUE}}
+    point.addMutation(mutation)
+    expect(point.mutations().length).to.equal(1)
+    // TODO: ^^ Fix Violation of Law of Demeter
+    expect(point.val()).to.deep.equal(SECOND_POINT_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::should error on trying to be redo a mutation that hasn't
+     been undone PT2, and point val should still be the same`, (t) => {
+    expect(() => point.redo(FIRST_MUTATION_INDEX)).to.throw(RangeError)
+    t.pass()
+})
+test(`Point > Mutable:::undoing the second mutation should make point equivalent
+     to the original value second mutation`, (t) => {
+    point.undo(FIRST_MUTATION_INDEX)
+    expect(point.val()).to.deep.equal(FIRST_POINT_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::redoing the second mutation should make point equivalent
+     to the second point value`, (t) => {
+    point.redo(FIRST_MUTATION_INDEX)
+    expect(point.val()).to.deep.equal(SECOND_POINT_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::adding the third and fourth and fifth mutation should make the point
+    equivalent to the fifth point value`, (t) => {
+    const thirdMutation = {
+        data: {delta: SECOND_MUTATION_VALUE},
+        timestamp: Date.now(),
+        type: PointMutationTypes.SHIFT,
+    }
+    const fourthMutation = {
+        data: {delta: THIRD_MUTATION_VALUE},
+        timestamp: Date.now(),
+        type: PointMutationTypes.SHIFT,
+    }
+    const fifthMutation = {
+        data: {delta: FOURTH_MUTATION_VALUE},
+        timestamp: Date.now(),
+        type: PointMutationTypes.SHIFT,
+    }
+    point.addMutation(thirdMutation)
+    point.addMutation(fourthMutation)
+    point.addMutation(fifthMutation)
+    expect(point.val()).to.deep.equal(FIFTH_POINT_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::undoing the second and fourth mutations should recalculate
+     the state as the sum of the 1st, third, and 5th mutations`, (t) => {
+    point.undo(FIRST_MUTATION_INDEX)
+    point.undo(THIRD_MUTATION_INDEX)
+    expect(point.val()).to.deep.equal(FIFTH_MINUS_1ST_AND_3RD_MUTATIONS_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::redoing the second and fourth mutations should recalculate
+     the state as the sum of the 1st, third, fourth, and 5th mutations`, (t) => {
+    point.redo(THIRD_MUTATION_INDEX)
+    expect(point.val()).to.deep.equal(FIFTH_MINUS_1ST_MUTATION_VALUE)
+    t.pass()
+})
+test(`Point > Mutable:::should error on trying to be redo a mutation that hasn't
+     been undone (or that has been redone),
+     and point val should still be the same`, (t) => {
+    expect(() => point.redo(THIRD_MUTATION_INDEX)).to.throw(RangeError)
+    t.pass()
+})
 
-        subscribableMutablePoint.onUpdate(callback)
-        subscribableMutablePoint.addMutation(mutation)
-        expect(callback.callCount).to.equal(1)
-    })
-    it('Adding a mutation, should trigger an update for multiple subscribers ', () => {
-        const FIRST_POINT_VALUE = {x: 5, y: 7}
-        const MUTATION_VALUE = {x: 3, y: 4}
-        const subscribableMutablePoint: IMutableSubscribablePoint
-            = new MutableSubscribablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
-        const callback1 = sinon.spy() // (updates: IDetailedUpdates) => void 0
-        const callback2 = sinon.spy() // (updates: IDetailedUpdates) => void 0
-        const mutation: IDatedMutation<PointMutationTypes> = {
-            timestamp: Date.now(),
-            type: PointMutationTypes.SHIFT,
-            data: {delta: {...MUTATION_VALUE}},
-        }
-        subscribableMutablePoint.onUpdate(callback1)
-        subscribableMutablePoint.onUpdate(callback2)
-        subscribableMutablePoint.addMutation(mutation)
-        expect(callback1.callCount).to.equal(1)
-        expect(callback2.callCount).to.equal(1)
-    })
+test('Point > Subscribable:::Adding a mutation, should trigger an update for one of the subscribers ', (t) => {
+    const MUTATION_VALUE = {x: 3, y: 4}
+    const subscribableMutablePoint: IMutableSubscribablePoint
+        = new MutableSubscribablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
+    const callback = sinon.spy() // (updates: IDetailedUpdates) => void 0
+    const mutation: IDatedMutation<PointMutationTypes> = {
+        timestamp: Date.now(),
+        type: PointMutationTypes.SHIFT,
+        data: {delta: {...MUTATION_VALUE}},
+    }
+
+    subscribableMutablePoint.onUpdate(callback)
+    subscribableMutablePoint.addMutation(mutation)
+    expect(callback.callCount).to.equal(1)
+    t.pass()
+})
+test('Point > Subscribable:::Adding a mutation, should trigger an update for multiple subscribers ', (t) => {
+    const MUTATION_VALUE = {x: 3, y: 4}
+    const subscribableMutablePoint: IMutableSubscribablePoint
+        = new MutableSubscribablePoint({updatesCallbacks: [], ...FIRST_POINT_VALUE})
+    const callback1 = sinon.spy() // (updates: IDetailedUpdates) => void 0
+    const callback2 = sinon.spy() // (updates: IDetailedUpdates) => void 0
+    const mutation: IDatedMutation<PointMutationTypes> = {
+        timestamp: Date.now(),
+        type: PointMutationTypes.SHIFT,
+        data: {delta: {...MUTATION_VALUE}},
+    }
+    subscribableMutablePoint.onUpdate(callback1)
+    subscribableMutablePoint.onUpdate(callback2)
+    subscribableMutablePoint.addMutation(mutation)
+    expect(callback1.callCount).to.equal(1)
+    expect(callback2.callCount).to.equal(1)
+    t.pass()
 })
--- app/objects/proficiencyStats/proficiencyStatsUtils.spec.ts
+++ app/objects/proficiencyStats/proficiencyStatsUtils.spec.ts
@@ -2,82 +2,81 @@
 // tslint:disable object-literal-sort-keys
 import {expect} from 'chai'
 
+import test from 'ava'
 import {IProficiencyStats} from '../interfaces';
 import {addObjToProficiencyStats} from './proficiencyStatsUtils';
 
-describe('addObjToProficiencyStats', () => {
-
-    it('should add a standalone ONE object to the current object', () => {
-        const proficiencyStats: IProficiencyStats = {
-            UNKNOWN: 5,
-            ONE: 4,
-            TWO: 1,
-            THREE: 4,
-            FOUR: 2,
-        }
-        const ONE = {ONE: 1}
-        const answer: IProficiencyStats = {
-            UNKNOWN: 5,
-            ONE: 5,
-            TWO: 1,
-            THREE: 4,
-            FOUR: 2,
-        }
-        const attemptedAnswer = addObjToProficiencyStats(proficiencyStats, ONE as IProficiencyStats)
-        expect(attemptedAnswer).to.deep.equal(answer)
-
-    })
-    it('should correctly add another object to the current object 1', () => {
-        const proficiencyStats: IProficiencyStats  = {
-            UNKNOWN: 5,
-            ONE: 4,
-            TWO: 1,
-            THREE: 4,
-            FOUR: 2,
-        }
-        const obj: IProficiencyStats  = {
-            UNKNOWN: 3,
-            ONE: 1,
-            TWO: 4,
-            THREE: 6,
-            FOUR: 2,
-        }
-        const answer: IProficiencyStats  = {
-            UNKNOWN: 8,
-            ONE: 5,
-            TWO: 5,
-            THREE: 10,
-            FOUR: 4,
-        }
-        expect(addObjToProficiencyStats(proficiencyStats, obj)).to.deep.equal(answer)
-
-    })
-    it('should correctly add another object to the current object 1', () => {
-        const proficiencyStats: IProficiencyStats  = {
-            UNKNOWN: 0,
-            ONE: 4,
-            TWO: 1,
-            THREE: 4,
-            FOUR: 2,
-        }
-        const obj: IProficiencyStats  = {
-            UNKNOWN: 3,
-            ONE: 1,
-            TWO: 4,
-            THREE: 6,
-            FOUR: 0,
-        }
-        const answer: IProficiencyStats  = {
-            UNKNOWN: 3,
-            ONE: 5,
-            TWO: 5,
-            THREE: 10,
-            FOUR: 2,
-        }
-        expect(addObjToProficiencyStats(proficiencyStats, obj)).to.deep.equal(answer)
-    })
-
+test('addObjToProficiencyStats::::should add a standalone ONE object to the current object', (t) => {
+    const proficiencyStats: IProficiencyStats = {
+        UNKNOWN: 5,
+        ONE: 4,
+        TWO: 1,
+        THREE: 4,
+        FOUR: 2,
+    }
+    const ONE = {ONE: 1}
+    const answer: IProficiencyStats = {
+        UNKNOWN: 5,
+        ONE: 5,
+        TWO: 1,
+        THREE: 4,
+        FOUR: 2,
+    }
+    const attemptedAnswer = addObjToProficiencyStats(proficiencyStats, ONE as IProficiencyStats)
+    expect(attemptedAnswer).to.deep.equal(answer)
+    t.pass()
 })
-describe('incrementProficiencyStatsCategory', () => {
-    // it('')
+test('addObjToProficiencyStats::::should correctly add another object to the current object 1', (t) => {
+    const proficiencyStats: IProficiencyStats  = {
+        UNKNOWN: 5,
+        ONE: 4,
+        TWO: 1,
+        THREE: 4,
+        FOUR: 2,
+    }
+    const obj: IProficiencyStats  = {
+        UNKNOWN: 3,
+        ONE: 1,
+        TWO: 4,
+        THREE: 6,
+        FOUR: 2,
+    }
+    const answer: IProficiencyStats  = {
+        UNKNOWN: 8,
+        ONE: 5,
+        TWO: 5,
+        THREE: 10,
+        FOUR: 4,
+    }
+    expect(addObjToProficiencyStats(proficiencyStats, obj)).to.deep.equal(answer)
+    t.pass()
 })
+test('addObjToProficiencyStats::::should correctly add another object to the current object 1', (t) => {
+    const proficiencyStats: IProficiencyStats  = {
+        UNKNOWN: 0,
+        ONE: 4,
+        TWO: 1,
+        THREE: 4,
+        FOUR: 2,
+    }
+    const obj: IProficiencyStats  = {
+        UNKNOWN: 3,
+        ONE: 1,
+        TWO: 4,
+        THREE: 6,
+        FOUR: 0,
+    }
+    const answer: IProficiencyStats  = {
+        UNKNOWN: 3,
+        ONE: 5,
+        TWO: 5,
+        THREE: 10,
+        FOUR: 2,
+    }
+    expect(addObjToProficiencyStats(proficiencyStats, obj)).to.deep.equal(answer)
+    t.pass()
+})
+
+// describe('incrementProficiencyStatsCategory', () => {
+//     // test('')
+// })
