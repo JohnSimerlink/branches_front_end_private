9f7ecf30bd4d36a3f13e2927de14caba2594636d
JohnSimerlink <johnsimerlink@gmail.com>
2018-01-04 17:25:41 -0500

    Init Forgetting Curve Calculator Widget

---
+++ forget/app/bootstrap.ts
@@ -0,0 +1,12 @@
+import {log} from '../../app/core/log'
+import Vue from 'vue'
+const vm = new Vue({
+    el: '#forgettingCalculator',
+    created() {
+        log('forgettingCalculator created')
+    },
+    data() {
+
+    },
+
+})
