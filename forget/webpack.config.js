97ab77f9aad2a7990fa46a3737f0d26ded23639d
JohnSimerlink <johnsimerlink@gmail.com>
2018-01-05 12:11:21 -0500

    Mark resolved merge conflict on coverage text files

---
+++ forget/webpack.config.js
@@ -0,0 +1,116 @@
+var path = require('path')
+var webpack = require('webpack')
+var CompressionPlugin = require("compression-webpack-plugin");
+var CodeAndTestConfig = require('../webpack.config.codeandtest.rules.js')
+module.exports = {
+    entry: {
+        regenerator: 'babel-regenerator-runtime',
+        build: './app/bootstrap.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: '[name].js'
+    },
+    // build: {
+    //   assetsPublicPath: '/',
+    //   assetsSubDirectory: 'static',
+    // },
+    module: {
+        rules: [
+            ...CodeAndTestConfig.rules,
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }          // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: CodeAndTestConfig.extensions
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        inline: true,
+        overlay: true,
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map',
+    // plugins: [new webpack.]
+
+
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+
+        new webpack.optimize.UglifyJsPlugin(), //minify everything
+        new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
+
+
+
+        // Added as the last plugin
+        // Not sure if it's worth gzipping index.html - no harm no foul
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+
+        // new webpack.DefinePlugin({
+        //   'process.env': {
+        //     NODE_ENV: '"production"'
+        //   }
+        // }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //   sourceMap: true,
+        //   compress: {
+        //     warnings: false
+        //   }
+        // }),
+        // new webpack.LoaderOptionsPlugin({
+        //   minimize: true
+        // })
+    ])
+}
