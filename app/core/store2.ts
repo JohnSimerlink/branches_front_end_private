394ae72cb7aea0862eda5bf04e2e3e4ba3f5d358
JohnSimerlink <johnsimerlink@gmail.com>
2018-01-17 12:59:21 -0500

    Use es2015 constant named functions for store mutation names

--- app/core/store2.ts
+++ app/core/store2.ts
@@ -59,74 +59,74 @@ const getters = {
 const mutations = {
     initializeSigmaInstance() {
 
-    }
-}
-mutations[MUTATION_NAMES.JUMP_TO] = (state, treeId) => {
-    state.jumpToId = treeId
-}
-mutations[MUTATION_NAMES.INITIALIZE_SIGMA_INSTANCE] = state => {
-    const sigmaInstance /*: Sigma*/ = new sigma({
-        graph: state.graphData,
-        container: GRAPH_CONTAINER_ID,
-        glyphScale: 0.7,
-        glyphFillColor: '#666',
-        glyphTextColor: 'white',
-        glyphStrokeColor: 'transparent',
-        glyphFont: 'FontAwesome',
-        glyphFontStyle: 'normal',
-        glyphTextThreshold: 6,
-        glyphThreshold: 3,
-    } as any/* as SigmaConfigs*/) as any
-    log('sigma truly just initialized')
-    state.sigmaInstance = sigmaInstance
-    state.graph = sigmaInstance.graph
-    state.sigmaInitialized = true
-    if (typeof window !== 'undefined') { // for debuggin only. NOT to be used by other classes
-        const windowAny: any = window
-        windowAny.sigmaInstance = sigmaInstance
-    }
+    },
+    [MUTATION_NAMES.JUMP_TO](state, treeId) {
+        state.jumpToId = treeId
+    },
+    [MUTATION_NAMES.INITIALIZE_SIGMA_INSTANCE](state) {
+        const sigmaInstance /*: Sigma*/ = new sigma({
+            graph: state.graphData,
+            container: GRAPH_CONTAINER_ID,
+            glyphScale: 0.7,
+            glyphFillColor: '#666',
+            glyphTextColor: 'white',
+            glyphStrokeColor: 'transparent',
+            glyphFont: 'FontAwesome',
+            glyphFontStyle: 'normal',
+            glyphTextThreshold: 6,
+            glyphThreshold: 3,
+        } as any/* as SigmaConfigs*/) as any
+        log('sigma truly just initialized')
+        state.sigmaInstance = sigmaInstance
+        state.graph = sigmaInstance.graph
+        state.sigmaInitialized = true
+        if (typeof window !== 'undefined') { // for debuggin only. NOT to be used by other classes
+            const windowAny: any = window
+            windowAny.sigmaInstance = sigmaInstance
+        }
 
-    sigmaInstance.cameras[0].goTo({x: 5, y: 5, ratio: .05})
-    /* TODO: it would be nice if I didn't have to do all this constructing
-     inside of store2.ts and rather did it inside of appContainer or inversify.config.ts */
-    const tooltips = sigmaAny.plugins.tooltips(sigmaInstance, sigmaInstance.renderers[0], tooltipsConfig)
-    const tooltipOpener: ITooltipOpener = new TooltipOpener({tooltips, store: getters['getStore']})
-    const sigmaEventListener: ISigmaEventListener = new SigmaEventListener({tooltipOpener, sigmaInstance})
-    sigmaEventListener.startListening()
-}
-mutations[MUTATION_NAMES.REFRESH] = state => {
-    log('store mutation refresh called', state)
-    state.sigmaInstance.refresh()
-}
-mutations[MUTATION_NAMES.ADD_NODE] = (state, node) => {
-    if (state.sigmaInitialized) {
-        log('sigma was already intiialized initialized . .. adding node', node)
-        state.graph.addNode(node)
-        mutations[MUTATION_NAMES.REFRESH](state)
-        log('STORE 2 TS ADD NODE. THIS STORE is ', getters['getStore']())
-    } else {
-        log('sigma not yet initialized . .. pushing node', node)
-        state.graphData.nodes.push(node)
-    }
-}
+        sigmaInstance.cameras[0].goTo({x: 5, y: 5, ratio: .05})
+        /* TODO: it would be nice if I didn't have to do all this constructing
+         inside of store2.ts and rather did it inside of appContainer or inversify.config.ts */
+        const tooltips = sigmaAny.plugins.tooltips(sigmaInstance, sigmaInstance.renderers[0], tooltipsConfig)
+        const tooltipOpener: ITooltipOpener = new TooltipOpener({tooltips, store: getters['getStore']})
+        const sigmaEventListener: ISigmaEventListener = new SigmaEventListener({tooltipOpener, sigmaInstance})
+        sigmaEventListener.startListening()
+    },
+    [MUTATION_NAMES.REFRESH](state) {
+        log('store mutation refresh called', state)
+        state.sigmaInstance.refresh()
+    },
+    [MUTATION_NAMES.ADD_NODE](state, node) {
+        if (state.sigmaInitialized) {
+            log('sigma was already initialized . .. adding node', node)
+            state.graph.addNode(node)
+            this[MUTATION_NAMES.REFRESH](state)
+            log('STORE 2 TS ADD NODE. THIS STORE is ', getters['getStore']())
+        } else {
+            log('sigma not yet initialized . .. pushing node', node)
+            state.graphData.nodes.push(node)
+        }
+    },
 // TODO: if contentUser does not yet exist in the DB create it.
-mutations[MUTATION_NAMES.ADD_CONTENT_INTERACTION] = (state, {userId, contentId, proficiency, timestamp}) => {
-    const contentUserId = getContentUserId({userId, contentId})
-    const id = contentUserId
-    const objectType = ObjectTypes.CONTENT_USER
-    const propertyName = ContentUserPropertyNames.PROFICIENCY;
-    const type = FieldMutationTypes.SET;
-    const data = proficiency
+    [MUTATION_NAMES.ADD_CONTENT_INTERACTION](state, {userId, contentId, proficiency, timestamp}) {
+        const contentUserId = getContentUserId({userId, contentId})
+        const id = contentUserId
+        const objectType = ObjectTypes.CONTENT_USER
+        const propertyName = ContentUserPropertyNames.PROFICIENCY;
+        const type = FieldMutationTypes.SET;
+        const data = proficiency
 
-    const storeMutation: IIdProppedDatedMutation<FieldMutationTypes, ContentUserPropertyNames> = {
-        data, id, propertyName, timestamp, type
-    }
+        const storeMutation: IIdProppedDatedMutation<FieldMutationTypes, ContentUserPropertyNames> = {
+            data, id, propertyName, timestamp, type
+        }
 
-    const globalMutation: IGlobalDatedMutation<FieldMutationTypes> = {
-        objectType,
-        ...storeMutation
+        const globalMutation: IGlobalDatedMutation<FieldMutationTypes> = {
+            objectType,
+            ...storeMutation
+        }
+        state.globalDataStore.addMutation(globalMutation)
     }
-    state.globalDataStore.addMutation(globalMutation)
 }
 const actions = {}
 
