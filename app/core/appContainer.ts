42e40fd080f90e0b1cffad50acb982909caeecea
JohnSimerlink <johnsimerlink@gmail.com>
2018-01-29 15:06:06 -0500

    Remove unused code from appContainer

--- app/core/appContainer.ts
+++ app/core/appContainer.ts
@@ -1,49 +1,25 @@
-// for now, this is where we will inject all the dependencies
 import {
-    IApp, IContentLoader, IContentUserLoader, IHash, IVueComponentCreator, IMutable, IMutableSubscribableContentStore,
-    IMutableSubscribableTreeUserStore, IOneToManyMap,
+    IApp,
     IRenderedNodesManager,
-    IRenderedNodesManagerCore, ISigma, ISigmaEventListener, ISigmaNode,
-    ISigmaRenderManager, ISigmaUpdater, IStoreSourceUpdateListener, IStoreSourceUpdateListenerCore,
+    ISigmaRenderManager, IStoreSourceUpdateListener,
     ISubscribableContentStoreSource, ISubscribableContentUserStoreSource,
     ISubscribableTreeLocationStoreSource,
-    ISubscribableTreeStoreSource, ITooltipOpener,
-    ITreeLoader, ITreeLocationLoader, ITreeUserLoader, ITreeComponentCreator2, ITree3Creator, IObjectFirebaseAutoSaver,
-    INewTreeComponentCreator, ISigmaNodes, IVueConfigurer
+    ISubscribableTreeStoreSource,
+    IVueConfigurer
 } from '../objects/interfaces';
 
 import {
-    ISigmaNodesUpdater,
     IUI,
     ISubscribableTreeUserStoreSource,
 } from '../objects/interfaces';
 import {CanvasUI} from '../objects/sigmaNode/CanvasUI';
-import {SigmaNodesUpdater, SigmaNodesUpdaterArgs} from '../objects/sigmaNode/SigmaNodesUpdater';
 import {App} from './app';
-import BranchesStore, {BranchesStoreArgs, MUTATION_NAMES} from './store2'
-
-import Vue from 'vue';
-import VueRouter from 'vue-router';
 import {Store} from 'vuex';
 import {myContainer} from '../../inversify.config';
-import {KnawledgeMapCreator, KnawledgeMapCreatorArgs} from '../components/knawledgeMap/knawledgeMap2';
-import {RenderedNodesManager} from '../objects/sigmaNode/RenderedNodesManager';
-import {RenderedNodesManagerCore, RenderedNodesManagerCoreArgs} from '../objects/sigmaNode/RenderedNodesManagerCore';
 import {TYPES} from '../objects/types';
 import {log, error} from './log'
-import {SigmaUpdater} from '../objects/sigmaUpdater/sigmaUpdater';
 import {configureSigma} from '../objects/sigmaNode/configureSigma';
 import sigma from '../../other_imports/sigma/sigma.core.js'
-import {StoreSourceUpdateListenerCore} from '../objects/stores/StoreSourceUpdateListenerCore';
-import {StoreSourceUpdateListener} from '../objects/stores/StoreSourceUpdateListener';
-const sigmaAny: any = sigma
-import StudyMenu from '../components/studyMenu/studyMenu'
-import {Tree2ComponentCreator} from '../components/tree2Component/treeComponent'
-import ItemHistory from '../components/itemHistory/itemHistory'
-import ProficiencySelector from '../components/proficiencySelector/proficiencySelector'
-// import NewTree from ''
-import {Tree3Creator} from '../components/tree3Component/tree3Component';
-import {NewTreeComponentCreator} from '../components/newTree/newTreeComponentCreator';
 import {partialInject} from '../testHelpers/partialInject';
 import {inject, injectable, tagged} from 'inversify';
 import {TAGS} from '../objects/tags';
@@ -61,6 +37,7 @@ export class AppContainer {
     private store: Store<any>
     private renderedNodesManager: IRenderedNodesManager
     private sigmaRenderManager: ISigmaRenderManager
+    private storeSourceUpdateListener: IStoreSourceUpdateListener
     constructor(@inject(TYPES.AppContainerArgs){
         contentStoreSource,
         contentUserStoreSource,
@@ -71,6 +48,7 @@ export class AppContainer {
         store,
         renderedNodesManager,
         sigmaRenderManager,
+        storeSourceUpdateListener,
    }: AppContainerArgs) {
         this.contentStoreSource = contentStoreSource
         this.contentUserStoreSource = contentUserStoreSource
@@ -81,6 +59,7 @@ export class AppContainer {
         this.store = store
         this.renderedNodesManager = renderedNodesManager
         this.sigmaRenderManager = sigmaRenderManager
+        this.storeSourceUpdateListener = storeSourceUpdateListener
     }
     // this should be the only place where I have new statements
     // and I should only have new statements here . . .
@@ -92,57 +71,17 @@ export class AppContainer {
 
     */
     public async start() {
-        // const sigmaRenderManager: ISigmaRenderManager =
-        // myContainer.get<ISigmaRenderManager>(TYPES.ISigmaRenderManager)
         this.renderedNodesManager.subscribe(this.sigmaRenderManager)
         // TODO: << ^^^ this should somehow be handled in ui.start or canvasui.start or something
-        const contentIdSigmaIdMap: IOneToManyMap<string>
-            // = myContainer.get<IOneToManyMap<string>>(TYPES.IOneToManyMap)
-            = myContainer.getTagged<IOneToManyMap<string>>(TYPES.IOneToManyMap, TAGS.CONTENT_ID_SIGMA_IDS_MAP, true)
-        // const me = this
-        const refresh = () => this.store.commit(MUTATION_NAMES.REFRESH)
-        const sigmaNodesUpdater: ISigmaNodesUpdater
-        = partialInject<SigmaNodesUpdaterArgs>({
-            konstructor: SigmaNodesUpdater,
-            constructorArgsType: TYPES.SigmaNodesUpdaterArgs,
-            injections: {
-                // sigmaRenderManager: this.sigmaRenderManager,
-                // getSigmaIdsForContentId: contentIdSigmaIdMap.get.bind(contentIdSigmaIdMap),
-            },
-            container: myContainer,
-        })
 
-        const storeSourceUpdateListenerCore: IStoreSourceUpdateListenerCore
-            // = new StoreSourceUpdateListenerCore({sigmaNodes, sigmaNodesUpdater, contentIdSigmaIdMap})
-            = partialInject<IStoreSourceUpdateListenerCore>({
-            konstructor: StoreSourceUpdateListenerCore,
-            constructorArgsType: TYPES.IStoreSourceUpdateListenerCore,
-            injections: {
-                // sigmaNodesUpdater,
-                // contentIdSigmaIdMap,
-            },
-            container: myContainer,
-        })
-        const storeSourceUpdateListener: IStoreSourceUpdateListener
-            = new StoreSourceUpdateListener({storeSourceUpdateListenerCore})
-
-        storeSourceUpdateListener.subscribe(this.treeStoreSource)
-        storeSourceUpdateListener.subscribe(this.treeLocationStoreSource)
-        storeSourceUpdateListener.subscribe(this.treeUserStoreSource)
-        storeSourceUpdateListener.subscribe(this.contentStoreSource)
-        storeSourceUpdateListener.subscribe(this.contentUserStoreSource)
+        this.storeSourceUpdateListener.subscribe(this.treeStoreSource)
+        this.storeSourceUpdateListener.subscribe(this.treeLocationStoreSource)
+        this.storeSourceUpdateListener.subscribe(this.treeUserStoreSource)
+        this.storeSourceUpdateListener.subscribe(this.contentStoreSource)
+        this.storeSourceUpdateListener.subscribe(this.contentUserStoreSource)
 
         const canvasUI: IUI =
             myContainer.get<CanvasUI>(TYPES.CanvasUI)
-            // new CanvasUI(
-            // {sigmaNodesUpdater}
-            // )
-        // const
-        // DataLoader.start()
-
-        // const globalStore: IMutableSubscribableGlobalStore
-        // = myContainer.get<IMutableSubscribableGlobalStore>(TYPES.IMutableSubscribableGlobalStore)
-        // const app: IApp = myContainer.get<IApp>(TYPES.IApp)
         const app: IApp =
             // new App({store: globalStore, UIs: [canvasUI]})
         partialInject<IApp>({
@@ -157,10 +96,6 @@ export class AppContainer {
         this.vueConfigurer.configure()
 
         // For now, new Vue must be called after app.start()
-
-        // TODO: don't make the app container container be fetched thorugh dependency injection until we have the UI
-        // manually wired and up
-        // and running with db saves and everything. Then define the dependency graph inside of the inversify config
     }
 
 }
@@ -182,4 +117,6 @@ export class AppContainerArgs {
     @inject(TYPES.ISigmaRenderManager)
     @tagged(TAGS.MAIN_SIGMA_INSTANCE, true)
         public sigmaRenderManager: ISigmaRenderManager
+    @inject(TYPES.IStoreSourceUpdateListener)
+        public storeSourceUpdateListener: IStoreSourceUpdateListener
 }
