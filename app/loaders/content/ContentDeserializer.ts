3f8ccf4315adb3dc1caa82b9811cca48dad3bd81
JohnSimerlink <johnsimerlink@gmail.com>
2018-01-31 13:26:28 -0500

    LoadFamily now works

--- app/loaders/content/ContentDeserializer.ts
+++ app/loaders/content/ContentDeserializer.ts
@@ -1,9 +1,12 @@
 import {MutableSubscribableField} from '../../objects/field/MutableSubscribableField';
 import {
     IHash, IMutableSubscribableContent, IContentData, CONTENT_TYPES,
-    ISyncableMutableSubscribableContent
+    ISyncableMutableSubscribableContent, IContentDataFromDB
 } from '../../objects/interfaces';
 import {SyncableMutableSubscribableContent} from '../../objects/content/SyncableMutableSubscribableContent';
+import {isValidContentUserDataFromDB} from '../../objects/contentUser/ContentUserValidator';
+import {isValidContentDataFromDB} from '../../objects/content/contentValidator';
+import {TreeDeserializer} from '../tree/TreeDeserializer';
 
 export class ContentDeserializer {
    public static deserialize(
@@ -21,4 +24,26 @@ export class ContentDeserializer {
            )
        return content
    }
+   public static convertContentDataFromDBToApp(
+       {contentDataFromDB}: {contentDataFromDB: IContentDataFromDB}): IContentData {
+      const contentData: IContentData = {
+          type: contentDataFromDB.type.val,
+          question: contentDataFromDB.question && contentDataFromDB.question.val,
+          answer: contentDataFromDB.answer && contentDataFromDB.answer.val,
+          title: contentDataFromDB.title && contentDataFromDB.title.val,
+      }
+      return contentData
+   }
+    public static deserializeFromDB(
+        {contentDataFromDB, contentId}: {contentDataFromDB: IContentDataFromDB, contentId: string}
+    ): ISyncableMutableSubscribableContent {
+       if (!isValidContentDataFromDB(contentDataFromDB)) {
+           throw new Error('Cannot deserialize content from db with id of ' + contentId
+               + ' and value of ' + contentDataFromDB)
+       }
+       const contentData: IContentData = ContentDeserializer.convertContentDataFromDBToApp({contentDataFromDB})
+       const content: ISyncableMutableSubscribableContent
+            = ContentDeserializer.deserialize({contentData, contentId})
+       return content
+    }
 }
