7e78843d7e603c2398ae9e7b637e48c221213c5c
JohnSimerlink <johnsimerlink@gmail.com>
2018-02-15 15:59:42 -0500

    remove api from front end

--- api/branches_footer.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const paymentHandler = require('./payment-handler');
-
-// Server side dependencies: stripe, express
-app.get('/', paymentHandler);
-app.listen(3000, () => console.log('Example app listening on port 3000!')); 
