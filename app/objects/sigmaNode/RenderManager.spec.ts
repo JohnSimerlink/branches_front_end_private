6e8eb53a2b74c9ff3f1c050de585b763f2000e15
JohnSimerlink <johnsimerlink@gmail.com>
2018-02-20 09:18:59 -0500

    Enforce consistent naming

--- app/objects/sigmaNode/RenderManager.spec.ts
+++ app/objects/sigmaNode/RenderManager.spec.ts
@@ -3,14 +3,14 @@ injectFakeDom()
 import test from 'ava'
 import {expect} from 'chai'
 import * as sinon from 'sinon'
-import {IRenderedNodesManager, ISigmaRenderManager} from '../interfaces';
+import {IRenderManager, ISigmaRenderManager} from '../interfaces';
 import {TYPES} from '../types';
 import {log} from '../../core/log'
 import {myContainer, myContainerLoadAllModules,} from '../../../inversify.config';
 import {TAGS} from '../tags';
 
 myContainerLoadAllModules()
-test('RenderedNodesManager::::subscribe should add RenderedNodesManagerCore.addNodeToRenderList' +
+test('RenderManager::::subscribe should add RenderManagerCore.addNodeToRenderList' +
     ' to obj\'s callback list', (t) => {
     // log('html element is', HTMLElement)
     // expect(sample('5')).to.equal(false)
@@ -18,8 +18,8 @@ test('RenderedNodesManager::::subscribe should add RenderedNodesManagerCore.addN
     const subscribable = myContainer.getTagged<ISigmaRenderManager>(
         TYPES.ISigmaRenderManager, TAGS.MAIN_SIGMA_INSTANCE, true
     )
-    const renderedNodesManager: IRenderedNodesManager
-        = myContainer.get<IRenderedNodesManager>(TYPES.IRenderedNodesManager)
+    const renderedNodesManager: IRenderManager
+        = myContainer.get<IRenderManager>(TYPES.IRenderedNodesManager)
     const subscribableOnUpdateSpy = sinon.spy(subscribable, 'onUpdate')
     renderedNodesManager.subscribe(subscribable)
     expect(subscribableOnUpdateSpy.callCount).to.equal(1)
