b089e61d11b37636b6d7dd076c9e6914c4cac6c3
JohnSimerlink <johnsimerlink@gmail.com>
2018-02-26 17:18:29 -0500

    WIP NewChildTree integration test

--- app/objects/content/contentUtils.ts
+++ app/objects/content/contentUtils.ts
@@ -1,4 +1,5 @@
 import {IContentData} from '../interfaces';
+import {log} from '../../core/log'
 let stringify = require('json-stable-stringify').default
 if (!stringify) {
     stringify = require('json-stable-stringify')
@@ -9,18 +10,20 @@ if (!md5) {
 }
 
 export function createContentId({question, answer, title, type}: IContentData) {
+    log('J14J - createContentId  called', {question, answer, title, type})
     const objectToStringify: IContentData = {} as IContentData
     objectToStringify.type = type
     if (question) {
         objectToStringify.question = question
     }
     if (answer) {
-        objectToStringify.question = question
+        objectToStringify.answer = answer
     }
     if (title) {
         objectToStringify.title = title
     }
     const stringified = stringify(objectToStringify)
     const contentId = md5(stringified)
+    log('J14J - createContentId  called', contentId)
     return contentId
 }
