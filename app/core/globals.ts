3d0b98af62be6e0e32cc89bf43c600b341e1ce35
JohnSimerlink <johnsimerlink@gmail.com>
2018-03-01 12:46:45 -0500

    Add partial implementation of BranchesMap Deserializer/Utils

--- app/core/globals.ts
+++ app/core/globals.ts
@@ -1,5 +1,6 @@
 export const GLOBAL_ROOT_ID: string = '1'
 export const DEFAULT_MAP_ID: string = '1'
+export const NON_EXISTENT_ID: string = 'nothing'
 export const ROOT_CONTENT_ID: string = '8904d53adfef7376627f4227ada47cd8'
 export const INITIAL_TREE_ID_TO_DOWNLOAD: string = GLOBAL_ROOT_ID
 export const ANOTHER_ID: string = '0544dddbb98b36dd9328eb71ba938465'
--- app/objects/content/contentUtils.ts
+++ app/objects/content/contentUtils.ts
@@ -9,6 +9,7 @@ if (!md5) {
     md5 = require('md5')
 }
 
+// TODO: make the id
 export function createContentId({question, answer, title, type}: IContentData) {
     log('J14J - createContentId  called', {question, answer, title, type})
     const objectToStringify: IContentData = {} as IContentData
@@ -21,6 +22,16 @@ export function createContentId({question, answer, title, type}: IContentData) {
     }
     if (title) {
         objectToStringify.title = title
+    }
+    const objectToStringifyFinal: any = objectToStringify
+    if (title) {
+        objectToStringifyFinal.random = Math.random() + Date.now()/* We need this field, because when people are creating titles, we need a different id for a Title of "History"
+         (under the category of "Africa" vs under the category of "Australia") */
+        /* Contrast this to a question/answer pair, where we always want the same question/answer pair to have the same id, regardless of what category it is under
+        e.g. "when was Georg Wash born?" "1722"
+        if under the American History category should have the same id as if it was a fact created under the Famous People category"
+
+         */
     }
     const stringified = stringify(objectToStringify)
     const contentId = md5(stringified)
