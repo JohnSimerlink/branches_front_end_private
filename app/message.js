e8b78fe5f67f3363656ac98b47304a8708ec6c03
Jon Seaman <jonmseaman@live.com>
2018-03-11 22:12:37 -0400

    Improve .gitignore

--- app/message.js
+++ app/message.js
@@ -3,17 +3,6 @@ if (typeof window !== 'undefined'){
     window.Snack = Snack
 }
 export default function message({text, color = 'white', duration = 2000, onclick = (snack) => {snack.hide()}}) {
-    //
-    // var snack = Snack.make("<your_message>", { /*<your_options>*/ });
-    // snack.show();
-
-//     console.log('Snack constructor is', Snack)
-//     var snack = new Snack();
-// snack.message ("Luke, I'm your father." + msg)
-//      .settings({ duration: Snack.LONG, hideOnClick: true })
-//      .show();
-
-
     var html =  "<div style='color: " + color + "'>" + text + "</div>"
     var snack = new Snack({
         domParent: document.querySelector('body'),
