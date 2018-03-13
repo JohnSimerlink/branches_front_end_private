734237e205ab8402c114c44955b23440291328b4
Jon Seaman <jonmseaman@live.com>
2018-03-13 16:17:28 -0400

    Terminate statements

--- app/loaders/treeLocation/TreeLocationDeserializer.spec.ts
+++ app/loaders/treeLocation/TreeLocationDeserializer.spec.ts
@@ -1,5 +1,5 @@
 import {injectFakeDom} from '../../testHelpers/injectFakeDom';
-injectFakeDom()
+injectFakeDom();
 import test from 'ava'
 import {expect} from 'chai'
 import 'reflect-metadata'
@@ -17,16 +17,16 @@ import {
     sampleTreeLocationDataFromFirebase1
 } from "../../objects/treeLocation/treeLocationTestHelpers";
 
-myContainerLoadAllModules({fakeSigma: true})
+myContainerLoadAllModules({fakeSigma: true});
 test('TreeLocationDeserializer::::Should deserializeFromDB properly with a blank mutation history' +
     ' (besides the mutation from creation)', (t) => {
     const sampleTreeLocation1: ISyncableMutableSubscribableTreeLocation
-        = getASampleTreeLocation1()
+        = getASampleTreeLocation1();
     const deserializedTreeLocation: IMutableSubscribableTreeLocation
-        = TreeLocationDeserializer.deserializeFromDB({treeLocationDataFromDB: sampleTreeLocationDataFromFirebase1})
-    expect(deserializedTreeLocation).to.deep.equal(sampleTreeLocation1)
+        = TreeLocationDeserializer.deserializeFromDB({treeLocationDataFromDB: sampleTreeLocationDataFromFirebase1});
+    expect(deserializedTreeLocation).to.deep.equal(sampleTreeLocation1);
     t.pass()
-})
+});
 // it('Should deserializeFromDB properly with a mutation history', () => {
 //
 //     const pointVal = {
