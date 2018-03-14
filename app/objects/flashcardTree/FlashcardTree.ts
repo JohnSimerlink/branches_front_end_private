9272fa2d4b9eb1ada28d5d59ba14de0172355501
JohnSimerlink <johnsimerlink@gmail.com>
2018-03-14 14:39:21 -0400

    Pass iterator test for a FlashcardTree with multiple levels

--- app/objects/flashcardTree/FlashcardTree.ts
+++ app/objects/flashcardTree/FlashcardTree.ts
@@ -3,33 +3,32 @@ import {IFlashcardTreeData} from './IFlashcardTreeData';
 import {id, IHash, IState} from '../interfaces';
 import {IFlashcardTreeArgs} from './IFlashcardTreeArgs'
 import {IFlashcardTree} from './IFlashcardTree'
+import {log} from '../../core/log'
 
 /**
  * Tree used for reviewing flashcards.
  */
 export class FlashcardTree implements IFlashcardTree {
     public data: IFlashcardTreeData;
-    private _children: IHash<IFlashcardTree>
+    public children: IHash<IFlashcardTree>
     constructor({
         data,
         children,
     }: IFlashcardTreeArgs ) {
         this.data = data
-        this._children = children
-
-    }
-    public children(): IHash<IFlashcardTree> {
-        return this._children
-    }
-    private hasChildren(): boolean {
-        return !!Object.keys(this.children()).length
+        this.children = children
     }
     /*
         An iterator that iterates through the data, not the other trees
      */
     public *[Symbol.iterator](): IterableIterator<IFlashcardTreeData> {
-        if (!this.hasChildren()) {
-            yield this.data
+        yield this.data
+        for (const key of Object.keys(this.children)){
+            const child = this.children[key]
+            yield* (Array.from(child) as IFlashcardTreeData[])
+            /* ^^ for more on why we need Array.from above,
+             see long comment starting on line 15 of FlashcardTree.spec.ts
+            */
         }
 
     }
