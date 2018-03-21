bf3b2e41f01cecf29b793d0353fd743e5b92f1ec
JohnSimerlink <johnsimerlink@gmail.com>
2018-03-21 14:54:15 -0400

    Fix sigmaNodesUpdater spec

---
+++ app/objects/treeUser/treeUserUtils.ts
@@ -0,0 +1,13 @@
+import {id} from '../interfaces';
+import {separator} from '../../loaders/contentUser/ContentUserLoaderUtils';
+
+export function getTreeId({treeUserId}: {treeUserId: id}) {
+    const treeId = treeUserId.substring(0, treeUserId.indexOf(separator));
+    return treeId;
+}
+export function getUserId({treeUserId}: {treeUserId: id}) {
+    const start = treeUserId.indexOf(separator) + separator.length;
+    const end = treeUserId.length;
+    const userId = treeUserId.substring(start, end);
+    return userId;
+}
