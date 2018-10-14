d95b6cd9dc5f27ff9468c0b2228757cfbdedc72d
JohnSimerlink <johnsimerlink@gmail.com>
2018-10-14 07:04:26 -0400

    Remove some compile errors

--- app/testHelpers/injectFakeDom.ts
+++ app/testHelpers/injectFakeDom.ts
@@ -2,8 +2,9 @@ import * as jsdom from 'jsdom-global';
 import {GRAPH_CONTAINER_ID} from '../core/globals';
 
 export function injectFakeDom() {
+	const jsdomAny: any = jsdom; // hack around TS error
 	const globalAny: any = global;
-	globalAny.cleanup = jsdom(`<!doctype html><html><head><meta charset="utf-8">' +
+	globalAny.cleanup = jsdomAny(`<!doctype html><html><head><meta charset="utf-8">' +
   '</head><body><div id='${GRAPH_CONTAINER_ID}'></div></body></html>`);
 	globalAny.requestAnimationFrame = () => {
 	};
--- webpack.config.productionandtest.rules.js
+++ webpack.config.productionandtest.rules.js
@@ -9,7 +9,7 @@ module.exports = {
             },
             {
                 test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
+                loader: 'ts-loader',
             },
             {
                 test: /\.js$/,
