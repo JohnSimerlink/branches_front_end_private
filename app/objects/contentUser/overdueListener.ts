39841e7e5e7ee6869343425d0b96436173e024fc
JohnSimerlink <johnsimerlink@gmail.com>
2018-10-22 19:19:59 -0400

    Fix bug in which items not overdue for another 24.8+ days were listed as overdue

--- app/objects/contentUser/overdueListener.ts
+++ app/objects/contentUser/overdueListener.ts
@@ -10,6 +10,7 @@ import {
 } from '../interfaces';
 import {log} from '../../core/log';
 
+const MAX_MILLISECONDS_FOR_TIMEOUT = 2147 * 1000 * 1000 // setInterval and setTimeout behavior becomes glitchy after surpassing 2^31 ish milliseconds which is around 2 bil seconds as listed above
 export class OverdueListener implements IOverdueListener {
 	private overdueListenerCore: IOverdueListenerCore;
 
@@ -50,34 +51,44 @@ export class OverdueListenerCore implements IOverdueListenerCore {
 	}
 
 	public listenAndReactToAnyNextReviewTimeChanges() {
-		this.nextReviewTime.onUpdate(newNextReviewTime => {
-			const markFalse: IDatedMutation<FieldMutationTypes> = {
-				timestamp: Date.now(),
-				type: FieldMutationTypes.SET,
-				data: false,
-			};
-			this.overdue.addMutation(markFalse);
-
-			clearTimeout(this.timeoutId);
-			this.setOverdueTimer();
-		});
+		this.nextReviewTime.onUpdate(this.onReviewTimeChange.bind(this))
 	}
 
 	public setOverdueTimer() {
 		const overdueTime = this.nextReviewTime.val();
 		const now = Date.now();
 		const millisecondsTilOverdue = overdueTime - now;
+		log('setOverdueTimer just called', millisecondsTilOverdue)
 		if (millisecondsTilOverdue <= 0) {
 			this.markOverdue();
 		} else {
+
+			this.markNotOverdue()
+			log('markNotOverdue just called')
+			if (millisecondsTilOverdue < MAX_MILLISECONDS_FOR_TIMEOUT)
 			this.timeoutId = window.setTimeout(() => {
 				/* ^^ deliberately say window.setTimeout (which returns a number,
 				as opposed to Node's setTimeout which returns type Timer. */
 				this.markOverdue();
+				log('window setTimeout of markOverDue just called')
 			}, millisecondsTilOverdue);
 		}
 	}
 
+	private onReviewTimeChange() {
+		log('onReviewTimeChange called')
+		clearTimeout(this.timeoutId);
+		this.markNotOverdue()
+		this.setOverdueTimer();
+	}
+	private markNotOverdue() {
+		const markFalse: IDatedMutation<FieldMutationTypes> = {
+			timestamp: Date.now(),
+			type: FieldMutationTypes.SET,
+			data: false,
+		};
+		this.overdue.addMutation(markFalse);
+	}
 	private markOverdue() {
 		const markTrue: IDatedMutation<FieldMutationTypes> = {
 			timestamp: Date.now(),
