f4aee26700bd7f1a0f35129f6d10324dd594b750
JohnSimerlink <johnsimerlink@gmail.com>
2019-03-27 23:14:32 -0400

    Split canvas nodes function into smaller function

--- other_imports/sigma/renderers/canvas/sigma.canvas.nodes.def.js
+++ other_imports/sigma/renderers/canvas/sigma.canvas.nodes.def.js
@@ -2,6 +2,7 @@ import sigma from '../../sigma.core'
 import {ProficiencyUtils} from '../../../../app/objects/proficiency/ProficiencyUtils'
 import {PROFICIENCIES} from "../../../../app/objects/proficiency/proficiencyEnum";
 
+// TODO: change all these sigma files to TS files
 const NODE_TYPES = {
     SHADOW_NODE: 9100,
 }
@@ -18,100 +19,120 @@ var Globals = {
     overdueSize: 2,
     regularSize: 1,
 }
-function proficiencyToColor(proficiency){
+
+function proficiencyToColor(proficiency) {
     if (proficiency > PROFICIENCIES.THREE) return Globals.colors.proficiency_4;
     if (proficiency > PROFICIENCIES.TWO) return Globals.colors.proficiency_3;
     if (proficiency > PROFICIENCIES.ONE) return Globals.colors.proficiency_2;
     if (proficiency > PROFICIENCIES.UNKNOWN) return Globals.colors.proficiency_1;
     return Globals.colors.proficiency_unknown;
 }
-  sigma.utils.pkg('sigma.canvas.nodes');
 
-  /**
-   * The default node renderer. It renders the node as a simple disc.
-   *
-   * @param  {object}                   node     The node object.
-   * @param  {CanvasRenderingContext2D} context  The canvas context.
-   * @param  {configurable}             settings The settings function.
-   */
-sigma.canvas.nodes.def = function(node, context, settings) {
-  // console.log("canvas nodes def called", node, context, settings)
-  if (node.type === NODE_TYPES.SHADOW_NODE){
-      return
-  }
+sigma.utils.pkg('sigma.canvas.nodes');
 
-  var prefix = settings('prefix') || '';
+/**
+ * The default node renderer. It renders the node as a simple disc.
+ *
+ * @param  {object}                   node     The node object.
+ * @param  {CanvasRenderingContext2D} context  The canvas context.
+ * @param  {configurable}             settings The settings function.
+ */
+sigma.canvas.nodes.def = function (node, context, settings) {
+    // console.log("canvas nodes def called", node, context, settings)
+    if (node.type === NODE_TYPES.SHADOW_NODE) {
+        return
+    }
+    const {size, x, y} = getDimensions(node, context, settings);
+    context.fillStyle = node.color || settings('defaultNodeColor');
+    context.font = "Nunito"
 
-  var size = node[prefix + 'size'];
-  context.fillStyle = node.color || settings('defaultNodeColor');
-  var x = node[prefix + 'x']
-  var y = node[prefix + 'y']
-  context.font="Nunito"
-  if (node.colorSlices) {
-      for (let colorSlice of node.colorSlices) {
-          // console.log("render for loop", colorSlice)
-          context.fillStyle = colorSlice.color
-          context.beginPath()
-          context.arc(
-              x,
-              y,
-              size,
-              colorSlice.start,
-              colorSlice.end,
-              true
-          )
-          context.closePath()
-          context.fill()
-      }
-  } else {
-      context.fillStyle = ProficiencyUtils.getColor(PROFICIENCIES.UNKNOWN)
-      context.beginPath()
-      context.arc(
-          x,
-          y,
-          size,
-          0,
-          2 * Math.PI,
-          true
-      )
-      context.closePath()
-      context.fill()
-  }
-
-  if (node.overdue){
-      var fontSize = Math.floor(size * 1.414)
-      context.font = fontSize + 'px Nunito'
-      // context.fillText('\uf017', x + size, y + size) // << TODO : Seems to only work on localhost . .. not on production
-      context.fillStyle = 'black'
-      context.fillText('⏱', x + size, y + size) //■►☼!⌚&#9200;&#8987', x + size, y + size)
-  }
-  var lineWidth = context.lineWidth
-  if (node.highlighted){
-      const circleRadius = size
-      // console.log('node highlighted. context and settings and this and node are', context, settings, this, node)
-
-      context.strokeStyle = 'blue'
-      context.lineWidth = circleRadius / 4
-      var center = context.beginPath()
-      context.fillStyle = 'blue'
-      context.arc(x, y, circleRadius, 0, Math.PI * 2, true);
-      context.stroke()
-  }
-  context.lineWidth = lineWidth
-
+    drawNode(context, node, size, x, y)
+    markNodeOverdueIfNecessary(context, node, size, x, y)
+    var lineWidth = context.lineWidth
+    highlightNodeIfNecessary(context, node, size, x, y)
+    context.lineWidth = lineWidth
 };
+function getDimensions(context, node, settings) {
+    var prefix = settings('prefix') || '';
+    return {
+        size: node[prefix + 'size'],
+        x: node[prefix + 'x'],
+        y: node[prefix + 'y'],
+    }
 
-function percentageToRadians(percentage){
-    return percentage * 2 * Math.PI
 }
 
-function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color){
+
+function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
     ctx.fillStyle = color;
 
     ctx.beginPath();
-    ctx.moveTo(centerX,centerY);
-    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle, true);
     ctx.closePath();
     ctx.fill();
+}
+function drawNode(context, node, size, x, y) {
+    if (node.colorSlices) {
+        for (let colorSlice of node.colorSlices) {
+            drawPieSlice(context, x, y, size, colorSlice.start, colorSlice.end, colorSlice.color)
+            // // console.log("render for loop", colorSlice)
+            // context.fillStyle = colorSlice.color
+            // context.beginPath()
+            // context.arc(
+            //     x,
+            //     y,
+            //     size,
+            //     colorSlice.start,
+            //     colorSlice.end,
+            //     true
+            // )
+            // context.closePath()
+            // context.fill()
+        }
+    } else {
+        context.fillStyle = ProficiencyUtils.getColor(PROFICIENCIES.UNKNOWN)
+        context.beginPath()
+        context.arc(
+            x,
+            y,
+            size,
+            0,
+            2 * Math.PI,
+            true
+        )
+        context.closePath()
+        context.fill()
+    }
+}
+
+function drawCreateNodeIfNecessary(context, node, size, x, y) {
+
+}
+
+function markNodeOverdueIfNecessary(context, node, size, x, y){
+    if (node.overdue) {
+        var fontSize = Math.floor(size * 1.414)
+        context.font = fontSize + 'px Nunito'
+        // context.fillText('\uf017', x + size, y + size) // << TODO : Seems to only work on localhost . .. not on production
+        context.fillStyle = 'black'
+        context.fillText('⏱', x + size, y + size) //■►☼!⌚&#9200;&#8987', x + size, y + size)
+    }
+}
+function highlightNodeIfNecessary(context, node, size, x, y) {
+    if (node.highlighted) {
+        const circleRadius = size
+        // console.log('node highlighted. context and settings and this and node are', context, settings, this, node)
+
+        context.strokeStyle = 'blue'
+        context.lineWidth = circleRadius / 4
+        var center = context.beginPath()
+        context.fillStyle = 'blue'
+        context.arc(x, y, circleRadius, 0, Math.PI * 2, true);
+        context.stroke()
+    }
+}
+
+function percentageToRadians(percentage) {
+    return percentage * 2 * Math.PI
 }
 
