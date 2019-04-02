919fb23ed78383fb8450e3ed31b42f61870991d2
JohnSimerlink <johnsimerlink@gmail.com>
2019-04-02 12:16:25 -0400

    Add a sample file to test http2_push

--- login.build.js
+++ login.build.js
@@ -1,1 +1,11 @@
-console.log('login.build.js called')
+console.log('login.build.js called', Date.now())
+setTimeout(() => {
+    console.log("about to load second script", Date.now())
+    loadScript('dist/build.js')
+}, 3000)
+function loadScript(path) {
+    const script = document.createElement('script')
+    script.setAttribute('src', path)
+    const body = document.querySelector('body')
+    body.appendChild(script);
+}
