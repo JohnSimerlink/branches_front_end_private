77a1abf2ca1f30bfe35a6fba3608454f9ffd3a9c
JohnSimerlink <johnsimerlink@gmail.com>
2020-06-05 08:35:06 -0400

    Firebase is now installing w/o error

--- app/objects/actionProcessor/actionProcessor.ts
+++ app/objects/actionProcessor/actionProcessor.ts
@@ -16,23 +16,23 @@ export function processUpdates() {
 
 }
 
-
-switchMany(mapInterationState, ['hoverCardIsSomething', 'editCardIsSomething', 'twoCardsAreSame', 'hoverCardFlipped', 'editCardFlipped'],
-  [
-    ([true, true, true, false, false]) => console.log("NO FHE. doing stuff"),))
-]
-matches(mapInteractionState,
-  ())
-hoverCardIsSomething: true
-editCardIsSomething: true
-twoCardsAreSame: true
-hoverCardFlipped: false
-editCardFlipped: false
-// function caseAction(o: T)
-type CaseAction<T> = (o: T) => any;
-function matches<T>(obj: T, caseActions: Array<CaseAction<T>>) {
-   for (const caseAction of caseActions) {
-
-
-   }
-}
+//
+// switchMany(mapInterationState, ['hoverCardIsSomething', 'editCardIsSomething', 'twoCardsAreSame', 'hoverCardFlipped', 'editCardFlipped'],
+//   [
+//     ([true, true, true, false, false]) => console.log("NO FHE. doing stuff"),))
+// ]
+// matches(mapInteractionState,
+//   ())
+// hoverCardIsSomething: true
+// editCardIsSomething: true
+// twoCardsAreSame: true
+// hoverCardFlipped: false
+// editCardFlipped: false
+// // function caseAction(o: T)
+// type CaseAction<T> = (o: T) => any;
+// function matches<T>(obj: T, caseActions: Array<CaseAction<T>>) {
+//    for (const caseAction of caseActions) {
+//
+//
+//    }
+// }
